0866e92aa34c3a1785a29560da18552d4d9b30be
Ben-Bingham <ben@benbingham.ca>
2022-06-20 11:11:23 -0600

    Initial project setup

--- CreateSolution.bat
+++ CreateSolution.bat
@@ -1,1 +1,6 @@
+DEL /S *.sln
+DEL /S *.vcxproj
+DEL /S *.vcxproj.filters
+DEL /S *.vcxproj.user
+
 CALL Dependencies\premake\premake5.exe vs2022
--- premake5.lua
+++ premake5.lua
@@ -2,73 +2,54 @@ workspace "Jade"
 	configurations {"Debug", "Release"}
 	platforms {"x32"}
 
+	startproject "Sandbox"
+
+	files {"**.cpp", "**.h"}
+
+	libdirs {
+		"Dependencies/GLFW/lib-vc2022",
+		"Dependencies/glew-2.1.0/lib/Release/Win32"
+	}
+
+	includedirs {
+			"Dependencies/GLFW/include",
+			"Dependencies/glew-2.1.0/include",
+			"Engine/include"
+	}
+
+	links {"glfw3", "glew32s", "opengl32"}
+
 	filter "platforms:x32"
 		architecture "x32"
 
-	filter {}
-
-	project "Engine"
-		kind "WindowedApp"
-		language "C++"
-		location "Engine"
-
-		includedirs {
-			"Engine/include/**",
-			"Dependencies/GLFW/include**",
-			"Dependencies/glew-2.1.0/include**"
-		}
-
-		files {"**.cpp", "**.h"}
-
-		libdirs {
-			"Dependencies/GLFW/lib-vc2022",
-			"Dependencies/glew-2.1.0/lib/Release/Win32"
-		}
-
-		links {"glfw3", "glew32s", "opengl32"}
-
-		targetdir ("Engine/build/bin")
-		objdir ("Engine/build/bin-int")
-
-		filter "configurations:Debug"
+	filter "configurations:Debug"
 			defines { "DEBUG", "GLEW_STATIC"}
 			symbols "On"
-
-		filter "configurations:Release"
-			defines { "NDEBUG", "GLEW_STATIC" }
-			optimize "On"
-
-		filter {}
+		
+	filter "configurations:Release"
+		defines { "NDEBUG", "GLEW_STATIC" }
+		optimize "On"
+		
+	filter {}
 
 	project "Sandbox"
-		kind "WindowedApp"
+		kind "ConsoleApp"
 		language "C++"
 		location "Sandbox"
 
 		includedirs {
-			"Engine/include/**",
-			"Dependencies/GLFW/include**",
-			"Dependencies/glew-2.1.0/include**"
+			"Sandbox/include"
 		}
 
-		files {"**.cpp", "**.h"}
-
-		libdirs {
-			"Dependencies/GLFW/lib-vc2022",
-			"Dependencies/glew-2.1.0/lib/Release/Win32"
-		}
-
-		links {"glfw3", "glew32s", "opengl32", "Engine"}
+		links {"Engine"}
 
 		targetdir ("Sandbox/build/bin")
 		objdir ("Sandbox/build/bin-int")
 
-		filter "configurations:Debug"
-			defines { "DEBUG", "GLEW_STATIC"}
-			symbols "On"
+	project "Engine"
+		kind "StaticLib"
+		language "C++"
+		location "Engine"
 
-		filter "configurations:Release"
-			defines { "NDEBUG", "GLEW_STATIC" }
-			optimize "On"
-
-		filter {}
+		targetdir ("Engine/build/bin")
+		objdir ("Engine/build/bin-int")
