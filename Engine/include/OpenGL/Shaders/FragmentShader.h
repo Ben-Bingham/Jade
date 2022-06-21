9fdf49bee26ebf1754c3b1db287e3355a8749b09
Ben-Bingham <ben@benbingham.ca>
2022-06-21 17:29:27 -0600

    split class into .cpp and .h files

--- Engine/include/OpenGL/Shaders/FragmentShader.h
+++ Engine/include/OpenGL/Shaders/FragmentShader.h
@@ -11,25 +11,7 @@ namespace Jade {
 		namespace Shaders {
 			class FragmentShader : public Shader {
 			public:
-
-				FragmentShader(Jade::Resources::TextFile& shaderSourceFile)
-					: m_Shader(glCreateShader(GL_FRAGMENT_SHADER)), m_ShaderSourceFile(shaderSourceFile) {
-
-					std::string shaderSource = m_ShaderSourceFile.getContent();
-					const char* charShaderSource = shaderSource.c_str();
-
-					glShaderSource(m_Shader, 1, &charShaderSource, NULL);
-					glCompileShader(m_Shader);
-
-					int success;
-					char infoLog[512];
-					glGetShaderiv(m_Shader, GL_COMPILE_STATUS, &success);
-					if (!success) {
-						glGetShaderInfoLog(m_Shader, 512, NULL, infoLog);
-						LOGGER.log("Fragment shader failed to compile", Jade::ERROR);
-						LOGGER.log(infoLog, Jade::ERROR);
-					}
-				}
+				FragmentShader(Jade::Resources::TextFile& shaderSourceFile);
 
 				~FragmentShader() {
 					dispose();
---
+++ Engine/src/OpenGl/Shaders/FragmentShader.cpp
@@ -0,0 +1,32 @@
+#include <GL/glew.h>
+
+#include "Jade.h"
+
+#include "OpenGL/Shaders/FragmentShader.h"
+
+#include "Resource Pipeline/TextFile.h"
+
+namespace Jade {
+	namespace OpenGL {
+		namespace Shaders {
+			FragmentShader::FragmentShader(Jade::Resources::TextFile& shaderSourceFile) 
+				: m_Shader(glCreateShader(GL_FRAGMENT_SHADER)), m_ShaderSourceFile(shaderSourceFile) {
+
+				std::string shaderSource = m_ShaderSourceFile.getContent();
+				const char* charShaderSource = shaderSource.c_str();
+
+				glShaderSource(m_Shader, 1, &charShaderSource, NULL);
+				glCompileShader(m_Shader);
+
+				int success;
+				char infoLog[512];
+				glGetShaderiv(m_Shader, GL_COMPILE_STATUS, &success);
+				if (!success) {
+					glGetShaderInfoLog(m_Shader, 512, NULL, infoLog);
+					LOGGER.log("Fragment shader failed to compile", Jade::ERROR);
+					LOGGER.log(infoLog, Jade::ERROR);
+				}
+			}
+		}
+	}
+}
