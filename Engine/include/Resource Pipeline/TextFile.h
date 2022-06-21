190f351fe0144441ccf105631024c54244ae682d
Ben-Bingham <ben@benbingham.ca>
2022-06-21 17:13:29 -0600

    Split textFile class into .cpp and .h and gave it a default constructo

--- Engine/include/Resource Pipeline/TextFile.h
+++ Engine/include/Resource Pipeline/TextFile.h
@@ -8,24 +8,13 @@ namespace Jade {
 	namespace Resources {
 		class TextFile {
 		public:
-			TextFile(std::string path) : m_Path(path) {
-				std::ifstream inputFileStream;
-
-				inputFileStream.open(m_Path);
-				if (!inputFileStream) {
-					LOGGER.log("Unable to open text file: " + m_Path, ERROR);
-				}
-
-				char character;
-				while (inputFileStream >> std::noskipws >> character) {
-					m_Content += character;
-				}
-
-				inputFileStream.close();
-			}
+			TextFile() {}
+			TextFile(std::string path) { setPath(path); }
 
 			std::string getContent() { return m_Content; }
-			std::string getPath() { return m_Path; }
+			std::string getPath() { return m_Path; } 
+			
+			void setPath(std::string newPath);
 
 		private:
 			std::string m_Path;
