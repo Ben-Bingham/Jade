11c9e2e9db14c14f364282c1791e05b7b32a9ac4
Ben-Bingham <ben@benbingham.ca>
2022-06-21 17:29:06 -0600

    split class into .cpp and .h files

--- Engine/include/OpenGL/Shaders/VertexShader.h
+++ Engine/include/OpenGL/Shaders/VertexShader.h
@@ -4,38 +4,20 @@
 #include "Jade.h"
 
 #include "Resource Pipeline/TextFile.h"
+#include "Shader.h"
 
 namespace Jade {
 	namespace OpenGL {
 		namespace Shaders {
 			class VertexShader : public Shader {
 			public:
-				VertexShader(Jade::Resources::TextFile& shaderSourceFile)
-					: m_Shader(glCreateShader(GL_VERTEX_SHADER)), m_ShaderSourceFile(shaderSourceFile) {
-
-					std::string shaderSource = m_ShaderSourceFile.getContent();
-					const char* charShaderSource = shaderSource.c_str();
-
-					glShaderSource(m_Shader, 1, &charShaderSource, NULL);
-					glCompileShader(m_Shader);
-
-					int success;
-					char infoLog[512];
-					glGetShaderiv(m_Shader, GL_COMPILE_STATUS, &success);
-					if (!success) {
-						glGetShaderInfoLog(m_Shader, 512, NULL, infoLog);
-						LOGGER.log("Vertex shader failed to compile", Jade::ERROR);
-						LOGGER.log(infoLog, Jade::ERROR);
-					}
-				}
+				VertexShader(Jade::Resources::TextFile& shaderSourceFile);
 
 				~VertexShader() {
 					dispose();
 				}
 
-				void dispose() override{
-					glDeleteShader(m_Shader);
-				}
+				void dispose() override{ glDeleteShader(m_Shader); }
 
 				unsigned int getShader() override{ return m_Shader; }
 
