af4602b7ad40b793a432147b33ed2ac2ae54a422
Ben-Bingham <ben@benbingham.ca>
2022-06-21 13:25:33 -0600

    Moved file into new folder

---
+++ Engine/include/OpenGL/Rendering Objects/VertexBufferObject.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <GL/glew.h>
+
+namespace Jade {
+	namespace OpenGL {
+		namespace RenderingObjects {
+			class VertexBufferObject {
+			public:
+				VertexBufferObject(float vertices[], int length) {
+					glGenBuffers(1, &m_VBO);
+		
+					bind();
+
+					glBufferData(GL_ARRAY_BUFFER, length, vertices, GL_STATIC_DRAW);
+				}
+
+				~VertexBufferObject() {
+					glDeleteBuffers(1, &m_VBO);
+				}
+
+				void bind() const{
+					glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+				}
+
+				static void unbind() {
+					glBindBuffer(GL_ARRAY_BUFFER, 0);
+				}
+
+				unsigned int getVBO() const { return m_VBO; }
+
+			private:
+				unsigned int m_VBO;
+			};
+
+		}
+	}
+}
