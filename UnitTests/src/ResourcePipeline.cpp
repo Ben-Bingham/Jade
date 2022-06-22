cb705abde8c01b27b5fe1b5a99b62e191fc54809
Ben-Bingham <ben@benbingham.ca>
2022-06-22 15:11:18 -0600

    Added unit tests for new image class

--- UnitTests/src/ResourcePipeline.cpp
+++ UnitTests/src/ResourcePipeline.cpp
@@ -1,7 +1,9 @@
 #include "Core Systems/Logging/Log.h"
 #include "UnitTests.h"
 #include "Core Systems/Resource Pipeline/TextFile.h"
+#include "Core Systems/Resource Pipeline/Image.h"
 
+// Text files -------------------------------------------------------------------------------------
 TEST_CASE("TextFile class can read text files", "[TextFile]") {
 
 	Jade::Resources::TextFile file("assets\\Test Text Document.txt");
@@ -18,4 +20,97 @@ TEST_CASE("TextFile class can read empty text files", "[TextFile]") {
 	std::string string = "\0";
 
 	REQUIRE(file.getContent() == string);
+}
+
+TEST_CASE("TextFile class can save path of file", "[TextFile]") {
+	std::string path = "assets\\Test Text Document2.txt";
+	Jade::Resources::TextFile file(path);
+
+	REQUIRE(file.getPath() == path);
+}
+
+
+// Images -----------------------------------------------------------------------------------------
+TEST_CASE("Image class can read Max alpha png images", "[Image]") {
+	unsigned char requiredData[] = {
+		255, 0  , 0  , 255,
+		255, 255, 0  , 255,
+		0  , 255, 0  , 255,
+		255, 0  , 255, 255,
+		255, 255, 255, 255,
+		0  , 255, 255, 255,
+		0  , 0  , 0  , 255,
+		255, 0  , 255, 255,
+		0  , 0  , 255, 255
+	};
+
+	Jade::Resources::Image image("assets\\pngTest.png");
+
+	for (int i = 0; i < 36; i++) {
+		REQUIRE(image.getContent()[i] == requiredData[i]);
+	}
+
+	image.dispose();
+}
+
+TEST_CASE("Image class can read transparent png images", "[Image]") {
+	unsigned char requiredData[] = {
+		130, 63 , 130, 255,
+		126, 87 , 194, 255,
+		101, 53 , 54 , 255,
+		42 , 54 , 177, 255,
+		150, 101, 21 , 139,
+		205, 220, 57 , 255,
+		33 , 33 , 33 , 0  ,
+		18 , 216, 18 , 109,
+		156, 204, 101, 255
+	};
+
+	Jade::Resources::Image image("assets\\pngTransparentTest.png");
+
+	for (int i = 0; i < 36; i++) {
+		REQUIRE(image.getContent()[i] == requiredData[i]);
+	}
+
+	image.dispose();
+}
+
+TEST_CASE("Image class can read number of channels, width and height from png images", "[Image]") {
+	Jade::Resources::Image image("assets\\pngTest.png");
+	REQUIRE(image.getChannels() == 4);
+	REQUIRE(image.getHeight() == 3);
+	REQUIRE(image.getWidth() == 3);
+
+	image.dispose();
+}
+
+TEST_CASE("Image class can read data from jpg images", "[Image]") {
+	unsigned char requiredData[] = {
+		167, 37 , 37 ,
+		253, 220, 189,
+		49 , 208, 115,
+		193, 64 , 95 ,
+		255, 239, 255,
+		102, 214, 198,
+		79 , 0  , 46 ,
+		148, 68 , 179,
+		1  , 18 , 160
+	};
+
+	Jade::Resources::Image image("assets\\jpgTest.jpg");
+
+	for (int i = 0; i < 27; i++) {
+		REQUIRE(image.getContent()[i] == requiredData[i]);
+	}
+
+	image.dispose();
+}
+
+TEST_CASE("Image class can read number of channels, width and height from jpg images", "[Image]") {
+	Jade::Resources::Image image("assets\\jpgTest.jpg");
+	REQUIRE(image.getChannels() == 3);
+	REQUIRE(image.getHeight() == 3);
+	REQUIRE(image.getWidth() == 3);
+
+	image.dispose();
 }
