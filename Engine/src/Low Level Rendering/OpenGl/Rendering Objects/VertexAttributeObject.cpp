ce9ce384abd50f26c20f62affcc9e69ab9d6c8b0
Ben-Bingham <ben@benbingham.ca>
2022-06-22 17:39:07 -0600

    Modified the vertex attribute class to use the proper offset

--- Engine/src/Low Level Rendering/OpenGl/Rendering Objects/VertexAttributeObject.cpp
+++ Engine/src/Low Level Rendering/OpenGl/Rendering Objects/VertexAttributeObject.cpp
@@ -6,41 +6,74 @@
 namespace Jade {
 	namespace OpenGL {
 		namespace RenderingObjects {
+			// Only use if there is a single attribute pointer. For multiple pointers use addAttributePointers(). (UNTESTED)
 			void VertexAttributeObject::addAttributePointer(VertexAttributePointer attributePointer) {
-				unsigned int offset = 0;
-				for (int i = 0; i < m_VertexArrtibutePointers.size(); i++) {
-					offset += m_VertexArrtibutePointers.at(i).stride;
-				}
+				bind();
 
 				glVertexAttribPointer(
-					m_VertexArrtibutePointers.size(),
+					0,
 					attributePointer.size,
 					attributePointer.type,
 					GL_FALSE,
-					attributePointer.size * sizeof(float),
-					(void*)offset
+					attributePointer.sizeOfType * attributePointer.size,
+					(void*)0
 				);
 
-				glEnableVertexAttribArray(m_VertexArrtibutePointers.size());
+				glEnableVertexAttribArray(0);
 
-				m_VertexArrtibutePointers.push_back(attributePointer);
+				m_VertexAttributePointers.push_back(attributePointer);
+			}
+
+			// If multiple attribute pointers are needed use this method.
+			void VertexAttributeObject::addAttributePointers(std::vector<VertexAttributePointer> attributePointers) {
+				m_VertexAttributePointers = std::move(attributePointers);
+
+				int totalStride = 0;
+
+				std::vector<VertexAttributePointer>::iterator it;
+				for (it = m_VertexAttributePointers.begin(); it != m_VertexAttributePointers.end(); it++) {
+					totalStride += (it->sizeOfType * it->size);
+				}
+
+				int count = 0;
+
+				for (it = m_VertexAttributePointers.begin(); it != m_VertexAttributePointers.end(); it++) {
+					int offset = 0;
+
+					for (int i = 0; i <= count - 1; i++) {
+						offset += (m_VertexAttributePointers.at(i).sizeOfType * m_VertexAttributePointers.at(i).size);
+					}
+
+					glVertexAttribPointer(
+						count,
+						it->size,
+						it->type,
+						GL_FALSE,
+						totalStride,
+						(void*)offset
+					);
+
+					glEnableVertexAttribArray(count);
+
+					count++;
+				}
 			}
 
 			VertexAttributePointer::VertexAttributePointer(unsigned int Size, GLenum Type) 
 				: size(Size), type(Type) {
 				switch (type) {
 				default:
-					stride = sizeof(float) * size;
+					sizeOfType = sizeof(float);
 					LOGGER.log("Unreckognized type for Vertex attribute pointer.", Jade::WARNING);
 					break;
 				case GL_FLOAT:
-					stride = sizeof(float) * size;
+					sizeOfType = sizeof(float);
 					break;
 				case GL_INT:
-					stride = sizeof(int) * size;
+					sizeOfType = sizeof(int);
 					break;
 				case GL_UNSIGNED_INT:
-					stride = sizeof(unsigned int) * size;
+					sizeOfType = sizeof(unsigned int);
 					break;
 				}
 			}
