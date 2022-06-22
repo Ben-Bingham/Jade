fd69930a794da3d73e38156f32554dec4803e665
Ben-Bingham <ben@benbingham.ca>
2022-06-22 14:32:58 -0600

    Text file now takes in temps and references

--- Engine/include/Core Systems/Resource Pipeline/TextFile.h
+++ Engine/include/Core Systems/Resource Pipeline/TextFile.h
@@ -9,12 +9,14 @@ namespace Jade {
 		class TextFile {
 		public:
 			TextFile() {}
-			TextFile(std::string path) { setPath(path); }
+			TextFile(std::string& path) { setPath(path); }
+			TextFile(std::string&& path) { setPath(path); }
 
 			std::string getContent() { return m_Content; }
 			std::string getPath() { return m_Path; } 
 			
-			void setPath(std::string newPath);
+			void setPath(std::string& newPath);
+			void setPath(std::string&& newPath);
 
 		private:
 			std::string m_Path;
