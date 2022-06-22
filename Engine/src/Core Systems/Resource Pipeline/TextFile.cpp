5bf7f07a1003e678b93e0aff3002b54a88556cf1
Ben-Bingham <ben@benbingham.ca>
2022-06-22 14:32:33 -0600

    Moved text file class

--- Engine/src/Core Systems/Resource Pipeline/TextFile.cpp
+++ Engine/src/Core Systems/Resource Pipeline/TextFile.cpp
@@ -7,7 +7,25 @@
 
 namespace Jade {
 	namespace Resources {
-		void TextFile::setPath(std::string newPath) {
+		void TextFile::setPath(std::string& newPath) {
+			m_Path = newPath;
+
+			std::ifstream inputFileStream;
+
+			inputFileStream.open(m_Path);
+			if (!inputFileStream) {
+				LOGGER.log("Unable to open text file: " + m_Path, ERROR);
+			}
+
+			char character;
+			while (inputFileStream >> std::noskipws >> character) {
+				m_Content += character;
+			}
+
+			inputFileStream.close();
+		}
+
+		void TextFile::setPath(std::string&& newPath) {
 			m_Path = newPath;
 
 			std::ifstream inputFileStream;
