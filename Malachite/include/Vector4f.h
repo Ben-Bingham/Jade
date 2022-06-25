b557887fe14c5c1d1e428c390db9b3cf9276646d
Ben-Bingham <ben@benbingham.ca>
2022-06-25 16:48:39 -0600

    Created basic Vector4f class to allow for vector matrix multiplication

---
+++ Malachite/include/Vector4f.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Vector3f.h"
+
+namespace Malachite {
+	class Vector4f {
+	public:
+		float x;
+		float y;
+		float z;
+		float w;
+
+		Vector4f() : x(0), y(0), z(0), w(0) {}
+		Vector4f(float value) : x(value), y(value), z(value), w(value) {}
+		Vector4f(float X, float Y, float Z, float W) : x(X), y(Y), z(Z), w(W) {}
+		Vector4f(const Vector3f& vector3, float W) : x(vector3.x), y(vector3.y), z(vector3.z), w(W) {}
+	};
+}
