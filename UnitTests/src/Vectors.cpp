e5564149eb6fa0c01f4ad708a3f561d5fc538dcf
Ben-Bingham <ben@benbingham.ca>
2022-06-26 11:23:19 -0600

    Added a method that compares floats and implemented it in tests

--- UnitTests/src/Vectors.cpp
+++ UnitTests/src/Vectors.cpp
@@ -1,5 +1,7 @@
 #include "UnitTests.h"
 #include "Vector3f.h"
+#include "Quaternion.h"
+#include "Malachite.h"
 
 using namespace Malachite;
 
@@ -18,9 +20,9 @@ TEST_CASE("Dot product can be succesfully calculated", "[Vector3f]") {
 	Vector3f vector2(3, 1, 4);
 	Vector3f vector3(4.687413f, 6.684684f, 3.656481f);
 
-	REQUIRE(dot(vector1, vector2) == 21.0f);
-	REQUIRE(dot(vector2, vector3) == 35.37285f);
-	//REQUIRE(dot(vector1, vector3) == 55.82956f); // FAILS but the numbers apear identical, floating point errors assumed
+	REQUIRE(compareFloats(dot(vector1, vector2), 21.0f));
+	REQUIRE(compareFloats(dot(vector2, vector3), 35.37285f));
+	REQUIRE(compareFloats(dot(vector1, vector3), 55.82956f));
 }
 
 TEST_CASE("Cross product can be succesfully calculated", "[Vector3f]") {
@@ -28,15 +30,16 @@ TEST_CASE("Cross product can be succesfully calculated", "[Vector3f]") {
 	Vector3f vector2(3, 1, 4);
 	Vector3f vector3(4.687413f, 6.684684f, 3.656481f);
 
-	REQUIRE(cross(vector1, vector2).x == 19.0f);
-	REQUIRE(cross(vector1, vector2).y == -13.0f);
-	REQUIRE(cross(vector1, vector2).z == -11.0f);
+	REQUIRE(compareFloats(cross(vector1, vector2).x, 19.0f));
+	REQUIRE(compareFloats(cross(vector1, vector2).y, -13.0f));
+	REQUIRE(compareFloats(cross(vector1, vector2).z, -11.0f));
 
-	REQUIRE(cross(vector2, vector3).x == -23.082255f);
-	//REQUIRE(cross(vector2, vector3).y == 7.78021f); // FAILS but the numbers apear identical, floating point errors assumed
-	//REQUIRE(cross(vector2, vector3).z == 15.366639f); // FAILS but the numbers apear identical, floating point errors assumed
+	REQUIRE(compareFloats(cross(vector2, vector3).x, -23.082255f));
+	REQUIRE(compareFloats(cross(vector2, vector3).y, 7.78021f)); 
+	REQUIRE(compareFloats(cross(vector2, vector3).z, 15.366639f));
 
-	//REQUIRE(cross(vector1, vector3).x == 11.59772f); // FAILS but the numbers apear identical, floating point errors assumed
-	REQUIRE(cross(vector1, vector3).y == -9.938511f);
-	REQUIRE(cross(vector1, vector3).z == 3.30167f);
+	REQUIRE(compareFloats(cross(vector1, vector3).x, 11.59772f));
+	REQUIRE(compareFloats(cross(vector1, vector3).y, -9.938511f));
+	REQUIRE(compareFloats(cross(vector1, vector3).z, 3.30167f));
+}
 }
