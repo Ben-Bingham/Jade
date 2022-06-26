7482d21560d4e7d2389c3307da3000e16ca6c96b
Ben-Bingham <ben@benbingham.ca>
2022-06-25 19:42:12 -0600

    Added Matrix on Matrix multiplication for the matrix class

--- Malachite/include/Matrix4f.h
+++ Malachite/include/Matrix4f.h
@@ -13,7 +13,76 @@ namespace Malachite {
 			m[2][0] = 0.0f; m[2][1] = 0.0f; m[2][2] = 1.0f; m[2][3] = 0.0f;
 			m[3][0] = 0.0f; m[3][1] = 0.0f; m[3][2] = 0.0f; m[3][3] = 1.0f;
 		}
+
+		Matrix4f(const Vector4f& row1, const Vector4f& row2, const Vector4f& row3, const Vector4f& row4) {
+			m[0][0] = row1.x; m[0][1] = row1.y; m[0][2] = row1.z; m[0][3] = row1.w;
+			m[1][0] = row2.x; m[1][1] = row2.y; m[1][2] = row2.z; m[1][3] = row2.w;
+			m[2][0] = row3.x; m[2][1] = row3.y; m[2][2] = row3.z; m[2][3] = row3.w;
+			m[3][0] = row4.x; m[3][1] = row4.y; m[3][2] = row4.z; m[3][3] = row4.w;
+		}
+
+		void scale(const Vector3f& scaleVector) {
+			Matrix4f scaleMatrix;
+			scaleMatrix.m[0][0] = scaleVector.x;
+			scaleMatrix.m[1][1] = scaleVector.y;
+			scaleMatrix.m[2][2] = scaleVector.z;
+			*this *= scaleMatrix;
+		}
+
+		Matrix4f& operator*=(const Matrix4f& other) {
+			m[0][0] = (m[0][0] * other.m[0][0]) + (m[0][1] * other.m[1][0]) + (m[0][2] * other.m[2][0]) + (m[0][3] * other.m[3][0]);
+			m[0][1] = (m[0][0] * other.m[0][1]) + (m[0][1] * other.m[1][1]) + (m[0][2] * other.m[2][1]) + (m[0][3] * other.m[3][1]);
+			m[0][2] = (m[0][0] * other.m[0][2]) + (m[0][1] * other.m[1][2]) + (m[0][2] * other.m[2][2]) + (m[0][3] * other.m[3][2]);
+			m[0][3] = (m[0][0] * other.m[0][3]) + (m[0][1] * other.m[1][3]) + (m[0][2] * other.m[2][3]) + (m[0][3] * other.m[3][3]);
+
+			// Second row
+			m[1][0] = (m[1][0] * other.m[0][0]) + (m[1][1] * other.m[1][0]) + (m[1][2] * other.m[2][0]) + (m[1][3] * other.m[3][0]);
+			m[1][1] = (m[1][0] * other.m[0][1]) + (m[1][1] * other.m[1][1]) + (m[1][2] * other.m[2][1]) + (m[1][3] * other.m[3][1]);
+			m[1][2] = (m[1][0] * other.m[0][2]) + (m[1][1] * other.m[1][2]) + (m[1][2] * other.m[2][2]) + (m[1][3] * other.m[3][2]);
+			m[1][3] = (m[1][0] * other.m[0][3]) + (m[1][1] * other.m[1][3]) + (m[1][2] * other.m[2][3]) + (m[1][3] * other.m[3][3]);
+								 
+			// Third row		 
+			m[2][0] = (m[2][0] * other.m[0][0]) + (m[2][1] * other.m[1][0]) + (m[2][2] * other.m[2][0]) + (m[2][3] * other.m[3][0]);
+			m[2][1] = (m[2][0] * other.m[0][1]) + (m[2][1] * other.m[1][1]) + (m[2][2] * other.m[2][1]) + (m[2][3] * other.m[3][1]);
+			m[2][2] = (m[2][0] * other.m[0][2]) + (m[2][1] * other.m[1][2]) + (m[2][2] * other.m[2][2]) + (m[2][3] * other.m[3][2]);
+			m[2][3] = (m[2][0] * other.m[0][3]) + (m[2][1] * other.m[1][3]) + (m[2][2] * other.m[2][3]) + (m[2][3] * other.m[3][3]);
+						
+			// Fourth row
+			m[3][0] = (m[3][0] * other.m[0][0]) + (m[3][1] * other.m[1][0]) + (m[3][2] * other.m[2][0]) + (m[3][3] * other.m[3][0]);
+			m[3][1] = (m[3][0] * other.m[0][1]) + (m[3][1] * other.m[1][1]) + (m[3][2] * other.m[2][1]) + (m[3][3] * other.m[3][1]);
+			m[3][2] = (m[3][0] * other.m[0][2]) + (m[3][1] * other.m[1][2]) + (m[3][2] * other.m[2][2]) + (m[3][3] * other.m[3][2]);
+			m[3][3] = (m[3][0] * other.m[0][3]) + (m[3][1] * other.m[1][3]) + (m[3][2] * other.m[2][3]) + (m[3][3] * other.m[3][3]);
+			return *this;
+		}
 	};
+
+	inline Matrix4f operator*(const Matrix4f& matrix1, const Matrix4f& matrix2) {
+		Matrix4f result;
+		// First row
+		result.m[0][0] = (matrix1.m[0][0] * matrix2.m[0][0]) + (matrix1.m[0][1] * matrix2.m[1][0]) + (matrix1.m[0][2] * matrix2.m[2][0]) + (matrix1.m[0][3] * matrix2.m[3][0]);
+		result.m[0][1] = (matrix1.m[0][0] * matrix2.m[0][1]) + (matrix1.m[0][1] * matrix2.m[1][1]) + (matrix1.m[0][2] * matrix2.m[2][1]) + (matrix1.m[0][3] * matrix2.m[3][1]);
+		result.m[0][2] = (matrix1.m[0][0] * matrix2.m[0][2]) + (matrix1.m[0][1] * matrix2.m[1][2]) + (matrix1.m[0][2] * matrix2.m[2][2]) + (matrix1.m[0][3] * matrix2.m[3][2]);
+		result.m[0][3] = (matrix1.m[0][0] * matrix2.m[0][3]) + (matrix1.m[0][1] * matrix2.m[1][3]) + (matrix1.m[0][2] * matrix2.m[2][3]) + (matrix1.m[0][3] * matrix2.m[3][3]);
+
+		// Second row
+		result.m[1][0] = (matrix1.m[1][0] * matrix2.m[0][0]) + (matrix1.m[1][1] * matrix2.m[1][0]) + (matrix1.m[1][2] * matrix2.m[2][0]) + (matrix1.m[1][3] * matrix2.m[3][0]);
+		result.m[1][1] = (matrix1.m[1][0] * matrix2.m[0][1]) + (matrix1.m[1][1] * matrix2.m[1][1]) + (matrix1.m[1][2] * matrix2.m[2][1]) + (matrix1.m[1][3] * matrix2.m[3][1]);
+		result.m[1][2] = (matrix1.m[1][0] * matrix2.m[0][2]) + (matrix1.m[1][1] * matrix2.m[1][2]) + (matrix1.m[1][2] * matrix2.m[2][2]) + (matrix1.m[1][3] * matrix2.m[3][2]);
+		result.m[1][3] = (matrix1.m[1][0] * matrix2.m[0][3]) + (matrix1.m[1][1] * matrix2.m[1][3]) + (matrix1.m[1][2] * matrix2.m[2][3]) + (matrix1.m[1][3] * matrix2.m[3][3]);
+
+		// Third row
+		result.m[2][0] = (matrix1.m[2][0] * matrix2.m[0][0]) + (matrix1.m[2][1] * matrix2.m[1][0]) + (matrix1.m[2][2] * matrix2.m[2][0]) + (matrix1.m[2][3] * matrix2.m[3][0]);
+		result.m[2][1] = (matrix1.m[2][0] * matrix2.m[0][1]) + (matrix1.m[2][1] * matrix2.m[1][1]) + (matrix1.m[2][2] * matrix2.m[2][1]) + (matrix1.m[2][3] * matrix2.m[3][1]);
+		result.m[2][2] = (matrix1.m[2][0] * matrix2.m[0][2]) + (matrix1.m[2][1] * matrix2.m[1][2]) + (matrix1.m[2][2] * matrix2.m[2][2]) + (matrix1.m[2][3] * matrix2.m[3][2]);
+		result.m[2][3] = (matrix1.m[2][0] * matrix2.m[0][3]) + (matrix1.m[2][1] * matrix2.m[1][3]) + (matrix1.m[2][2] * matrix2.m[2][3]) + (matrix1.m[2][3] * matrix2.m[3][3]);
+
+		// Fourth row
+		result.m[3][0] = (matrix1.m[3][0] * matrix2.m[0][0]) + (matrix1.m[3][1] * matrix2.m[1][0]) + (matrix1.m[3][2] * matrix2.m[2][0]) + (matrix1.m[3][3] * matrix2.m[3][0]);
+		result.m[3][1] = (matrix1.m[3][0] * matrix2.m[0][1]) + (matrix1.m[3][1] * matrix2.m[1][1]) + (matrix1.m[3][2] * matrix2.m[2][1]) + (matrix1.m[3][3] * matrix2.m[3][1]);
+		result.m[3][2] = (matrix1.m[3][0] * matrix2.m[0][2]) + (matrix1.m[3][1] * matrix2.m[1][2]) + (matrix1.m[3][2] * matrix2.m[2][2]) + (matrix1.m[3][3] * matrix2.m[3][2]);
+		result.m[3][3] = (matrix1.m[3][0] * matrix2.m[0][3]) + (matrix1.m[3][1] * matrix2.m[1][3]) + (matrix1.m[3][2] * matrix2.m[2][3]) + (matrix1.m[3][3] * matrix2.m[3][3]);
+		return result;
+	}
 
 	inline Vector4f& operator*(const Vector4f& vector4, const Matrix4f& matrix) {
 		Vector4f result;
--- UnitTests/src/Matricies.cpp
+++ UnitTests/src/Matricies.cpp
@@ -13,8 +13,54 @@ TEST_CASE("Blank matrix does not modifiy a vector", "[Matrix4f]") {
 	REQUIRE(result.z == vector.z);
 }
 
-TEST_CASE("Matrix can scale a vector", "[Matrix4f]") {
+TEST_CASE("Matrix can multiply another matrix", "[Matrix4f]") {
+	Vector4f row1(5, 7, 9, 10);
+	Vector4f row2(2, 3, 3, 8);
+	Vector4f row3(8, 10, 2, 3);
+	Vector4f row4(3, 3, 4, 8);
 
+	Matrix4f matrix1(row1, row2, row3, row4);
+
+	Vector4f row1_2(3, 10, 12, 18);
+	Vector4f row2_2(12, 1, 4, 9);
+	Vector4f row3_2(9, 10, 12, 2);
+	Vector4f row4_2(3, 12, 4, 10);
+
+	Matrix4f matrix2(row1_2, row2_2, row3_2, row4_2);
+
+	Matrix4f result = matrix1 * matrix2;
+
+	REQUIRE(result.m[0][0] == 210);
+	REQUIRE(result.m[0][1] == 267);
+	REQUIRE(result.m[0][2] == 236);
+	REQUIRE(result.m[0][3] == 271);
+
+	REQUIRE(result.m[1][0] == 93);
+	REQUIRE(result.m[1][1] == 149);
+	REQUIRE(result.m[1][2] == 104);
+	REQUIRE(result.m[1][3] == 149);
+
+	REQUIRE(result.m[2][0] == 171);
+	REQUIRE(result.m[2][1] == 146);
+	REQUIRE(result.m[2][2] == 172);
+	REQUIRE(result.m[2][3] == 268);
+
+	REQUIRE(result.m[3][0] == 105);
+	REQUIRE(result.m[3][1] == 169);
+	REQUIRE(result.m[3][2] == 128);
+	REQUIRE(result.m[3][3] == 169);
+
+}
+
+TEST_CASE("Matrix can scale a vector", "[Matrix4f]") {
+	Matrix4f matrix;
+	matrix.scale(Vector3f(2));
+	Vector3f vector(1, 2, 3);
+
+	Vector3f result = vector * matrix;
+	REQUIRE(result.x == 2);
+	REQUIRE(result.y == 4);
+	REQUIRE(result.z == 6);
 }
 
 TEST_CASE("Matrix can translate a vector", "[Matrix4f]") {
