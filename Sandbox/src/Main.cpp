b4611b867c5ca79054e4915758669cc2bc6beba8
Ben-Bingham <ben@benbingham.ca>
2022-06-26 17:27:32 -0600

    Added 3d cube and basic camera

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -35,29 +35,79 @@ int main() {
 		LOGGER.log("GLEW failed to initilize.", Jade::ERROR);
 	}
 
+	// OpenGL settings
+	glEnable(GL_DEPTH_TEST);
+
+	// Uncomment for wireframe rendering
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	
 	// Render init
 
 	// Vertex array object
 	VertexAttributeObject VAO;
 
 	// Vertex buffer object
+
 	float vertices[] = {
-	 // Vertex Positions     Texture cords
-		-0.5,  0.5,  0.0,	  0.0f, 1.0f,	// Top left
-		 0.5,  0.5,  0.0,	  1.0f, 1.0f,	// Top right
-		 0.5, -0.5,  0.0,	  1.0f, 0.0f,	// Bottom right
-		-0.5, -0.5,  0.0,	  0.0f, 0.0f	// Bottom left
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
+	//float vertices[] = {
+	// // Vertex Positions     Texture cords
+	//	-0.5,  0.5,  0.0,	  0.0f, 1.0f,	// Top left
+	//	 0.5,  0.5,  0.0,	  1.0f, 1.0f,	// Top right
+	//	 0.5, -0.5,  0.0,	  1.0f, 0.0f,	// Bottom right
+	//	-0.5, -0.5,  0.0,	  0.0f, 0.0f	// Bottom left
+	//};
 
 	VertexBufferObject VBO(vertices, sizeof(vertices));
 
 	// Element buffer object
-	unsigned int indices[] = {
+	/*unsigned int indices[] = {
 		0, 1, 2,
 		0, 2, 3
 	};
 
-	ElementBufferObject EBO(indices, sizeof(indices));
+	ElementBufferObject EBO(indices, sizeof(indices));*/
 
 	// Vertex attribute pointers
 	VertexAttributePointer positionData(3, GL_FLOAT);
@@ -92,8 +142,12 @@ int main() {
 	Texture::activateUnit(1);
 	texture2.bind();
 
-	// Uncomment for wireframe rendering
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	// Camera setup
+	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
 	// Check for errors
 	glCheckError();
@@ -108,13 +162,10 @@ int main() {
 		
 		window.pollEvents();
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-
-		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
 		shaderProgram.use();
 
@@ -123,7 +174,7 @@ int main() {
 		shaderProgram.setMatrix4f("projection", projection);
 
 		VAO.bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		window.swapBuffers();
 		glCheckError();
