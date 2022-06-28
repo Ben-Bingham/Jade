53dc02e8a09df1c7557b92b14c7f6731314a4ccf
Ben-Bingham <ben@benbingham.ca>
2022-06-28 17:24:43 -0600

    Logs removed

--- Engine/include/Core Systems/Game Objects System/Scene.h
+++ Engine/include/Core Systems/Game Objects System/Scene.h
@@ -10,18 +10,15 @@ namespace Jade {
 	class Scene {
 	public:
 		Scene() {
-			LOGGER.log("Scene created", Jade::INFO);
 		}
 
 		void render() {
-			LOGGER.log("Scene render", Jade::INFO);
 			/*for each (GameObject gameObject in m_GameObjects) {
 				gameObject.render();
 			}*/
 		}
 
 		void addGameObject(const GameObject& gameObject) {
-			LOGGER.log("Game object added to scene", Jade::INFO);
 			//m_GameObjects.push_back(gameObject);
 		}
 
