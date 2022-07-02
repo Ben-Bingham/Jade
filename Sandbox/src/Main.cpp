30b71279c9d6da2055c89d1713a4f3db5e56f62a
Ben-Bingham <ben@benbingham.ca>
2022-07-02 11:06:19 -0600

    Removed mesh from main

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -5,21 +5,17 @@
 
 #include "Core Systems/Game Objects System/GameObject.h"
 #include "Core Systems/Game Objects System/Scene.h"
-#include "Core Systems/Components/Mesh.h"
 
 #include "Jade.h"
 
 int main() {
+
 	Jade::Application app("Jade test");
 	app.Begin(); // must be called first
 
 	Jade::Scene scene;
 
 	Jade::GameObject testCube;
-
-	Jade::CubeMesh cubeMesh(testCube);
-	
-	testCube.addComponent(std::make_shared<Jade::CubeMesh>(std::move(cubeMesh)));
 
 	scene.addGameObject(testCube);
 
