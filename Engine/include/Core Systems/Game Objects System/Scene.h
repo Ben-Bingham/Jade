ee6efcc423bdb5ce459e384aca0f03b1b11d19b1
Ben-Bingham <ben@benbingham.ca>
2022-07-02 11:07:36 -0600

    Update Scene.h

--- Engine/include/Core Systems/Game Objects System/Scene.h
+++ Engine/include/Core Systems/Game Objects System/Scene.h
@@ -5,24 +5,29 @@
 #include "Jade.h"
 
 #include "GameObject.h"
+#include "Low Level Rendering/Shaders/ShaderProgram.h"
+#include <Core Systems/Components/Transform.h>
 
 namespace Jade {
 	class Scene {
 	public:
-		Scene() {
+		Scene() : m_GameObjects() {}
+
+		void render(const ShaderProgram& shaderProgram) {
+			//TODO upload other matricies
+
+			std::vector<GameObject>::iterator it;
+			for (it = m_GameObjects.begin(); it != m_GameObjects.end(); it++) {
+				it->render(shaderProgram);
+			}
 		}
 
-		void render() {
-			/*for each (GameObject gameObject in m_GameObjects) {
-				gameObject.render();
-			}*/
-		}
-
-		void addGameObject(const GameObject& gameObject) {
-			//m_GameObjects.push_back(gameObject);
+		void addGameObject(GameObject& gameObject) {
+			m_GameObjects.push_back(gameObject);
 		}
 
 	private:
 		std::vector<GameObject> m_GameObjects;
+
 	};
 }
