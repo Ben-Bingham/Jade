61d47c82d7691dbdbfc2dc1bfb548efe06c6bf45
Ben-Bingham <ben@benbingham.ca>
2022-07-02 11:06:37 -0600

    Created renderer class to replace mesh

---
+++ Engine/src/Core Systems/Components/Renderer.cpp
@@ -0,0 +1,82 @@
+#include <vector>
+
+#include "Core Systems/Components/Renderer.h"
+#include "Core Systems/Game Objects System/GameObject.h"
+#include "Core Systems/Components/Transform.h"
+#include "Low Level Rendering/Rendering Objects/VertexAttributeObject.h"
+
+namespace Jade {
+
+	std::vector<float> cubeVertices = {
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,	1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,	1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,	1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,	0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,	1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,	1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,	0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,	0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,	1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,	1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,	0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,	0.0f, 1.0f
+	};
+
+	void nullRender(GameObject& gameObject) {
+		std::string message = "The game object with ID: ";
+		message += std::to_string(gameObject.getID());
+		message += " does not have a render function asociated with it, and probably should.";
+		LOGGER.log(message, Jade::WARNING);
+	}
+
+	void cubeRender(GameObject& gameObject) {
+		//Bind VAO and draw
+		gameObject.renderer->getVAO();
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+
+	Renderer::Renderer(GameObject& gameObject, std::vector<float> verticies) 
+		: m_GameObject(gameObject), m_Verticies(verticies), m_VAO(), m_VBO(verticies) {
+
+		gameObject.setRenderFunction(nullRender);
+
+		VertexAttributePointer positionData(3, GL_FLOAT);
+		VertexAttributePointer texCordData(2, GL_FLOAT);
+
+		std::vector<VertexAttributePointer> attributePointers = {
+			positionData,
+			texCordData
+		};
+
+		m_VAO.addAttributePointers(attributePointers);
+	}
+}
