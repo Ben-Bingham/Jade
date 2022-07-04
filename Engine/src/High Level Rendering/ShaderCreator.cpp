734a9a3a4588b06c8449a7fc3243fd4a3f6a5c0b
Ben-Bingham <ben@benbingham.ca>
2022-07-04 13:09:38 -0600

    Modified string literal formatting

--- Engine/src/High Level Rendering/ShaderCreator.cpp
+++ Engine/src/High Level Rendering/ShaderCreator.cpp
@@ -3,14 +3,14 @@
 namespace Jade {
 	VertexShader ShaderCreator::vertexShaderInit() {
 		std::string vertexShaderSource =
-			"#version 330 core\n\
-\n\
-layout (location = 0) in vec3 inputPositon;\n\
-\n\
-void main()\n\
-{\n\
-	gl_Position = vec4(inputPositon, 1.0);\n\
-}\0";
+			"#version 330 core\n"
+			"\n"
+			"layout (location = 0) in vec3 inputPositon;\n"
+			"\n"
+			"void main()\n"
+			"{\n"
+			"	gl_Position = vec4(inputPositon, 1.0);\n"
+			"}\0";
 
 		std::string vertexShaderPath = "assets\\shaders\\ruleSet";
 		vertexShaderPath += std::to_string(m_RuleSet.getID());
@@ -21,14 +21,15 @@ void main()\n\
 
 	FragmentShader ShaderCreator::fragmentShaderInit() {
 		std::string fragmentShaderSource =
-			"#version 330 core\n\
-\n\
-out vec4 FragColor;\n\
-\n\
-void main()\n\
-{\n\
-	FragColor = vec4(1.0);\n\
-}\0";
+			"#version 330 core\n"
+			"\n"
+			"out vec4 FragColor;\n"
+			"\n"
+			"void main()\n"
+			"{\n"
+			"	FragColor = vec4(1.0);\n"
+			"}\0;";
+
 		std::string fragmentShaderPath = "assets\\shaders\\ruleSet";
 		fragmentShaderPath += std::to_string(m_RuleSet.getID());
 		fragmentShaderPath += ".frag";
