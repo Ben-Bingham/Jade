c95d63bdb1c740a1579fc58d903afbc2fb913e97
Ben-Bingham <ben@benbingham.ca>
2022-07-05 16:06:58 -0600

    Added more matrix stuff

--- Engine/include/High Level Rendering/Renderer.h
+++ Engine/include/High Level Rendering/Renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 #include "RenderingRuleSet.h"
 #include "RenderableObject.h"
@@ -8,7 +9,7 @@
 namespace Jade {
 	class Renderer {
 	public:
-		Renderer(const RenderingRuleSet& ruleSet, glm::mat4& viewMatrix, glm::mat4& projectionMatrix) 
+		Renderer(const RenderingRuleSet& ruleSet, const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix)
 			: m_RuleSet(ruleSet), m_View(viewMatrix), m_Projection(projectionMatrix) { }
 
 		void addRenderable(RenderableObject& renderable) {
@@ -19,26 +20,27 @@ namespace Jade {
 
 		void render() {
 			m_RuleSet.bind();
-			m_RuleSet.getProgram().setMatrix4f("view", m_View);				//upload view and projection matrixs
+			/*LOGGER.log(glm::to_string(m_View), INFO);
+			LOGGER.log(glm::to_string(m_Projection), INFO);*/
+			m_RuleSet.getProgram().setMatrix4f("view", m_View);
 			m_RuleSet.getProgram().setMatrix4f("projection", m_Projection);
 			
-			//upload any additional data needed for ruleset like lights
+			//upload any additional data needed for ruleset like lights TODO
 			std::vector<RenderableObject>::iterator it;
 			for (it = m_Renderables.begin(); it != m_Renderables.end(); it++) {
 				it->render(m_RuleSet);
 			}
-			//for each renderalbe {
-			//	render
-			//}
-			//
-			//
-			//
+		}
+
+		void setMatrices(const glm::mat4& view, const glm::mat4& projection) {
+			m_View = view;
+			m_Projection = projection;
 		}
 
 	private:
 		RenderingRuleSet m_RuleSet;
 		std::vector<RenderableObject> m_Renderables;
-		glm::mat4& m_View;
-		glm::mat4& m_Projection;
+		glm::mat4 m_View;
+		glm::mat4 m_Projection;
 	};
 }
