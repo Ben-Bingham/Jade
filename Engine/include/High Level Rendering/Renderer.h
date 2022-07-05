e63742132f64cca0526f0cf7ab5f6becefd5f433
Ben-Bingham <ben@benbingham.ca>
2022-07-05 09:41:52 -0600

    Renderer upload matricies

--- Engine/include/High Level Rendering/Renderer.h
+++ Engine/include/High Level Rendering/Renderer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <glm/glm.hpp>
 
 #include "RenderingRuleSet.h"
 #include "RenderableObject.h"
@@ -7,7 +8,8 @@
 namespace Jade {
 	class Renderer {
 	public:
-		Renderer(const RenderingRuleSet& ruleSet) : m_RuleSet(ruleSet) {}
+		Renderer(const RenderingRuleSet& ruleSet, glm::mat4& viewMatrix, glm::mat4& projectionMatrix) 
+			: m_RuleSet(ruleSet), m_View(viewMatrix), m_Projection(projectionMatrix) { }
 
 		void addRenderable(RenderableObject& renderable) {
 			if (renderable.followsRuleSet(m_RuleSet)) {
@@ -17,10 +19,26 @@ namespace Jade {
 
 		void render() {
 			m_RuleSet.bind();
+			m_RuleSet.getProgram().setMatrix4f("view", m_View);				//upload view and projection matrixs
+			m_RuleSet.getProgram().setMatrix4f("projection", m_Projection);
+			
+			//upload any additional data needed for ruleset like lights
+			std::vector<RenderableObject>::iterator it;
+			for (it = m_Renderables.begin(); it != m_Renderables.end(); it++) {
+				it->render(m_RuleSet);
+			}
+			//for each renderalbe {
+			//	render
+			//}
+			//
+			//
+			//
 		}
 
 	private:
 		RenderingRuleSet m_RuleSet;
 		std::vector<RenderableObject> m_Renderables;
+		glm::mat4& m_View;
+		glm::mat4& m_Projection;
 	};
 }
