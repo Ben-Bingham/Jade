e26b0b304b61f402f370ce17a367aecf5d4a7d34
Ben-Bingham <ben@benbingham.ca>
2022-07-05 18:50:38 -0600

    Moved 2 funciton definitions into the .cpp file

--- Engine/include/High Level Rendering/RenderableObject.h
+++ Engine/include/High Level Rendering/RenderableObject.h
@@ -21,46 +21,9 @@ namespace Jade {
 			m_VAO.setAttributePointer(positionData);
 		}
 
-		bool followsRuleSet(RenderingRuleSet& ruleSet) {
-			bool hasPosition = false;
-			bool hasNormals = false;
-			std::vector<VertexAttributePointer>::iterator it;
-			std::vector<VertexAttributePointer> attributes = m_VAO.getAttributePointers();
-			for (it = attributes.begin(); it != attributes.end(); it++) {
-				if (it->name == POSITION) {
-					hasPosition = true;
-				}
+		bool followsRuleSet(RenderingRuleSet& ruleSet);
 
-				if (it->name == NORMAL) {
-					hasNormals = true;
-				}
-			}
-
-			if (
-				ruleSet.Vertex_Normals == hasNormals &&
-				ruleSet.Vertex_Positions == hasPosition &&
-				(hasNormals && hasPosition) == ruleSet.Phong_Lighting
-				) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-
-		void render(const RenderingRuleSet& ruleSet) const {
-			ruleSet.getProgram().setVector3f("objectColour", m_ObjectColour);
-			ruleSet.getProgram().setMatrix4f("model", m_Transform.getMatrix());
-
-			m_VAO.bind();
-
-			int numberOfVerticies = 0;
-			switch (m_Shape) {
-			case CUBE: numberOfVerticies = 36; break;
-			}
-
-			glDrawArrays(GL_TRIANGLES, 0, numberOfVerticies);
-		}
+		void render(const RenderingRuleSet& ruleSet) const;
 
 		void dispose() { m_VAO.dispose(); }
 
--- Engine/src/High Level Rendering/RenderableObject.cpp
+++ Engine/src/High Level Rendering/RenderableObject.cpp
@@ -3,6 +3,47 @@
 #include "High Level Rendering/RenderableObject.h"
 
 namespace Jade {
+	bool RenderableObject::followsRuleSet(RenderingRuleSet& ruleSet) {
+		bool hasPosition = false;
+		bool hasNormals = false;
+		std::vector<VertexAttributePointer>::iterator it;
+		std::vector<VertexAttributePointer> attributes = m_VAO.getAttributePointers();
+		for (it = attributes.begin(); it != attributes.end(); it++) {
+			if (it->name == POSITION) {
+				hasPosition = true;
+			}
+
+			if (it->name == NORMAL) {
+				hasNormals = true;
+			}
+		}
+
+		if (
+			ruleSet.Vertex_Normals == hasNormals &&
+			ruleSet.Vertex_Positions == hasPosition &&
+			(hasNormals && hasPosition) == ruleSet.Phong_Lighting
+			) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void RenderableObject::render(const RenderingRuleSet& ruleSet) const {
+		ruleSet.getProgram().setVector3f("objectColour", m_ObjectColour);
+		ruleSet.getProgram().setMatrix4f("model", m_Transform.getMatrix());
+
+		m_VAO.bind();
+
+		int numberOfVerticies = 0;
+		switch (m_Shape) {
+		case CUBE: numberOfVerticies = 36; break;
+		}
+
+		glDrawArrays(GL_TRIANGLES, 0, numberOfVerticies);
+	}
+
 	std::vector<float> RenderableObject::cubeVerticies = {
 		-0.5f, -0.5f, -0.5f,
 		 0.5f, -0.5f, -0.5f,
