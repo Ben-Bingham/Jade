cf39a20bc51f18624268cc2ece8d88d46c166f39
Ben-Bingham <ben@benbingham.ca>
2022-07-06 14:00:32 -0600

    removed allows renderable function

--- Engine/src/High Level Rendering/RenderingRuleSet.cpp
+++ Engine/src/High Level Rendering/RenderingRuleSet.cpp
@@ -4,41 +4,13 @@
 #include <High Level Rendering/RenderableObject.h>
 
 namespace Jade {
-	RenderingRuleSet::RenderingRuleSet() : m_Program(programInit()), m_ID(getNextID()) {}
+	RenderingRuleSet::RenderingRuleSet(RuleSet rules) : m_Program(programInit()), m_ID(getNextID()), ruleSet(rules) {}
 
 	void RenderingRuleSet::createProgram() {
-		processRules();
 		m_Program.dispose();
 		ShaderCreator creator(*this);
 		m_Program = creator.createProgram();
 		creator.dispose();
-	}
-
-	bool RenderingRuleSet::allowsRenderable(RenderableObject& renderable) {
-		bool hasPosition = false;
-		bool hasNormals = false;
-		std::vector<VertexAttributePointer>::iterator it;
-		std::vector<VertexAttributePointer> attributes = renderable.getVAO().getAttributePointers();
-		for (it = attributes.begin(); it != attributes.end(); it++) {
-			if (it->name == POSITION) {
-				hasPosition = true;
-			}
-
-			if (it->name == NORMAL) {
-				hasNormals = true;
-			}
-		}
-
-		if (
-			this->Vertex_Normals == hasNormals &&
-			this->Vertex_Positions == hasPosition ||
-			(hasNormals && hasPosition) == this->Phong_Lighting
-			) {
-			return true;
-		}
-		else {
-			return false;
-		}
 	}
 
 	ShaderProgram RenderingRuleSet::programInit() {
