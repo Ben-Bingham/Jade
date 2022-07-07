514715595495cc918ddb758f6fbf3f43f8e988fa
Ben-Bingham <ben@benbingham.ca>
2022-07-07 17:38:19 -0600

    Created lightCreator class that creates lights with static functions

---
+++ Engine/include/High Level Rendering/LightCreator.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "Low Level Rendering/Shaders/ShaderStructs.h"
+
+namespace Jade {
+	class LightCreator {
+	public:
+		static PointLight DefaultPointLight() {
+			return PointLight{
+				glm::vec3(0.0f, 0.0f, 0.0f), // Position
+
+				1.0f,						 // Constant
+				0.09f,						 // Linear
+				0.32f,						 // Quadratic
+
+				glm::vec3(0.2f, 0.2f, 0.2f), // Ambient
+				glm::vec3(0.5f, 0.5f, 0.5f), // Diffuse
+				glm::vec3(1.0f, 1.0f, 1.0f)  // Specular
+			};
+		}
+	};
+}
