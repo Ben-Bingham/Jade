c1749da1f8635288d5bf0f94b16631658618e2bb
Ben-Bingham <ben@benbingham.ca>
2022-07-07 17:06:58 -0600

    Organized class and removed unnessacary comments

--- Engine/include/High Level Rendering/RenderingRuleSet.h
+++ Engine/include/High Level Rendering/RenderingRuleSet.h
@@ -25,37 +25,17 @@ namespace Jade {
 		virtual void bindAdditionals() = 0;
 
 		void createProgram();
-		
-
-		//void addLight(const Light& light) {
-		//	if (ruleSet == STANDARD || ruleSet == TEXTURE) {
-		//		m_Lights.push_back(light);
-		//	}
-		//	else {
-		//		std::string message = "The rule set with ID: ";
-		//		message += std::to_string(m_ID);
-		//		message += "does not support lights.";
-		//		LOGGER.log(message, Jade::WARNING);
-		//	}
-		//}
-
-		void dispose() {
-			m_Program.dispose();
-		}
-
-		//std::vector<Light> getLights() { return m_Lights; }
 
 		int getID() const { return m_ID; }
 
 		ShaderProgram getProgram() const { return m_Program; }
 
-		RuleSet ruleSet;
+		void dispose() { m_Program.dispose(); }
 
+		RuleSet ruleSet;
 	private:
 		int m_ID;
 		ShaderProgram m_Program;
-		//Light m_Light; //TODO allow multiple lights
-		//std::vector<Light> m_Lights;
 
 		static int ID;
 
