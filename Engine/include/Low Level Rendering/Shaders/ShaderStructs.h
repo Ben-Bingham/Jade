09bfa986f91eea550c60c1295285c42aee30fbd4
Ben-Bingham <ben@benbingham.ca>
2022-07-07 17:30:22 -0600

    Removed unnecessary comment

--- Engine/include/Low Level Rendering/Shaders/ShaderStructs.h
+++ Engine/include/Low Level Rendering/Shaders/ShaderStructs.h
@@ -3,14 +3,6 @@
 #include "glm/glm.hpp"
 
 namespace Jade {
-	//struct Light {
-	//	glm::vec3 position;
-
-	//	glm::vec3 ambient;
-	//	glm::vec3 diffuse;
-	//	glm::vec3 specular;
-	//};
-
 	struct Material {
 		glm::vec3 ambient;
 		glm::vec3 diffuse;
