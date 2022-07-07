03c6363cbab47bf189342e045375ffb383803a3b
Ben-Bingham <ben@benbingham.ca>
2022-07-07 17:15:04 -0600

    Created new structs for different types of lights

--- Engine/include/Low Level Rendering/Shaders/ShaderStructs.h
+++ Engine/include/Low Level Rendering/Shaders/ShaderStructs.h
@@ -3,13 +3,13 @@
 #include "glm/glm.hpp"
 
 namespace Jade {
-	struct Light {
-		glm::vec3 position;
+	//struct Light {
+	//	glm::vec3 position;
 
-		glm::vec3 ambient;
-		glm::vec3 diffuse;
-		glm::vec3 specular;
-	};
+	//	glm::vec3 ambient;
+	//	glm::vec3 diffuse;
+	//	glm::vec3 specular;
+	//};
 
 	struct Material {
 		glm::vec3 ambient;
@@ -17,4 +17,39 @@ namespace Jade {
 		glm::vec3 specular;
 		float shininess;
 	};
+
+    struct DirLight {
+        glm::vec3 direction;
+
+        glm::vec3 ambient;
+        glm::vec3 diffuse;
+        glm::vec3 specular;
+    };
+
+    struct PointLight {
+        glm::vec3 position;
+
+        float constant;
+        float linear;
+        float quadratic;
+
+        glm::vec3 ambient;
+        glm::vec3 diffuse;
+        glm::vec3 specular;
+    };
+
+    /*struct SpotLight { //TODO
+        glm::vec3 position;
+        glm::vec3 direction;
+        float cutOff;
+        float outerCutOff;
+
+        float constant;
+        float linear;
+        float quadratic;
+
+        glm::vec3 ambient;
+        glm::vec3 diffuse;
+        glm::vec3 specular;
+    };*/
 }
