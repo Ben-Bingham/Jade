26c94e74fd302a06f820ead387f8c6afbdf0498a
Ben-Bingham <ben@benbingham.ca>
2022-07-07 17:52:21 -0600

    Added function in lightCreator to create directional lights

--- Engine/include/High Level Rendering/LightCreator.h
+++ Engine/include/High Level Rendering/LightCreator.h
@@ -8,7 +8,7 @@ namespace Jade {
 	class LightCreator {
 	public:
 		static PointLight DefaultPointLight() {
-			return PointLight{
+			return PointLight {
 				glm::vec3(0.0f, 0.0f, 0.0f), // Position
 
 				1.0f,						 // Constant
@@ -18,6 +18,16 @@ namespace Jade {
 				glm::vec3(0.2f, 0.2f, 0.2f), // Ambient
 				glm::vec3(0.5f, 0.5f, 0.5f), // Diffuse
 				glm::vec3(1.0f, 1.0f, 1.0f)  // Specular
+			};
+		}
+
+		static DirectionalLight DefaultDirectionalLight() {
+			return DirectionalLight{
+				glm::vec3(-0.2f, -1.0f, -0.3f),	// Direction
+
+				glm::vec3(0.2f, 0.2f, 0.2f),	// Ambient
+				glm::vec3(0.2f, 0.2f, 0.2f),	// Diffuse
+				glm::vec3(1.0f, 1.0f, 1.0f)		// Specular
 			};
 		}
 	};
