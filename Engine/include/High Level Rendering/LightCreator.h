9d414bf692c8fc3426ab407f74ea21e530b152fd
Ben-Bingham <ben@benbingham.ca>
2022-07-10 16:23:40 -0600

    Corrected default values

--- Engine/include/High Level Rendering/LightCreator.h
+++ Engine/include/High Level Rendering/LightCreator.h
@@ -22,11 +22,11 @@ namespace Jade {
 		}
 
 		static DirectionalLight DefaultDirectionalLight() {
-			return DirectionalLight{
+			return DirectionalLight {
 				glm::vec3(-0.2f, -1.0f, -0.3f),	// Direction
 
 				glm::vec3(0.2f, 0.2f, 0.2f),	// Ambient
-				glm::vec3(0.2f, 0.2f, 0.2f),	// Diffuse
+				glm::vec3(0.5f, 0.5f, 0.5f),	// Diffuse
 				glm::vec3(1.0f, 1.0f, 1.0f)		// Specular
 			};
 		}
