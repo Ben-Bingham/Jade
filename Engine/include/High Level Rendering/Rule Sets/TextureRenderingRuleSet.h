2afd6f986bd897b5ebe45b2becd04ce0ab1015cf
Ben-Bingham <ben@benbingham.ca>
2022-07-10 16:43:13 -0600

    Created Texture Ruleset class

---
+++ Engine/include/High Level Rendering/Rule Sets/TextureRenderingRuleSet.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "High Level Rendering/Rule Sets/RenderingRuleSet.h"
+#include "High Level Rendering/LightCreator.h"
+
+namespace Jade {
+	class TextureRuleSet : public RenderingRuleSet {
+	public:
+		TextureRuleSet() : RenderingRuleSet(TEXTURE), m_DirectionalLight(LightCreator::DefaultDirectionalLight()) {}
+
+		void bindAdditionals() override {
+			getProgram().setPointLights("lights", m_PointLights);
+			getProgram().setFloat("numberOfLights", m_PointLights.size());
+			getProgram().setDirectionalLight("directionalLight", m_DirectionalLight);
+		}
+
+		void addPointLight(const PointLight& light) {
+			m_PointLights.push_back(light);
+		}
+
+		void setDirectionalLight(const DirectionalLight& light) {
+			m_DirectionalLight = light;
+		}
+
+		std::vector<PointLight> getPointLights() { return m_PointLights; }
+
+	private:
+		std::vector<PointLight> m_PointLights;
+		DirectionalLight m_DirectionalLight;
+	};
+
+}
