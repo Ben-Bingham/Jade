20a5f236ee7aedaf4eddca39677dd23546cc579d
Ben-Bingham <ben@benbingham.ca>
2022-07-10 19:42:14 -0600

    Made bind funciton const

--- Engine/include/Low Level Rendering/Rendering Objects/Texture.h
+++ Engine/include/Low Level Rendering/Rendering Objects/Texture.h
@@ -13,7 +13,7 @@ namespace Jade {
 		Texture(const Jade::Image& image);
 		Texture(const std::string& path);
 
-		void bind() { glBindTexture(GL_TEXTURE_2D, m_Texture); }
+		void bind() const { glBindTexture(GL_TEXTURE_2D, m_Texture); }
 		static void activateUnit(int unit) { glActiveTexture(GL_TEXTURE0 + unit); }
 
 	private:
