200be692288a6a378fad63158fa2b16449104ab5
Ben-Bingham <ben@benbingham.ca>
2022-07-11 11:15:49 -0600

    Removed unnecessary comments

--- Engine/include/Low Level Rendering/Shaders/ShaderStructs.h
+++ Engine/include/Low Level Rendering/Shaders/ShaderStructs.h
@@ -36,19 +36,4 @@ namespace Jade {
         Texture specular;
         float shininess;
     };
-
-    /*struct SpotLight { //TODO
-        glm::vec3 position;
-        glm::vec3 direction;
-        float cutOff;
-        float outerCutOff;
-
-        float constant;
-        float linear;
-        float quadratic;
-
-        glm::vec3 ambient;
-        glm::vec3 diffuse;
-        glm::vec3 specular;
-    };*/
 }
