499d87fba79a6c58765c9aa34ab3a3ce51bf5c2d
Ben-Bingham <ben@benbingham.ca>
2022-07-11 11:11:33 -0600

    Removed unnecessary comment

--- Engine/src/High Level Rendering/RenderableObject.cpp
+++ Engine/src/High Level Rendering/RenderableObject.cpp
@@ -3,27 +3,6 @@
 #include "High Level Rendering/Renderable Objects/RenderableObject.h"
 
 namespace Jade {
-	/*void RenderableObject::render(const RenderingRuleSet& ruleSet) const {
-		if (ruleSet.ruleSet == SOLID_COLOUR) {
-			ruleSet.getProgram().setVector4f("objectColour", m_ObjectColour);
-		}
-        else if (ruleSet.ruleSet == TEXTURE) {
-            ruleSet.getProgram().setMaterial("material", m_Material);
-        }
-		else {
-			ruleSet.getProgram().setMaterial("material", m_Material);
-		}
-
-        getTexture().bind();
-        Texture::activateUnit(0);
-
-		ruleSet.getProgram().setMatrix4f("model", m_Transform.getMatrix());
-
-		m_VAO.bind();
-		
-		glDrawArrays(GL_TRIANGLES, 0, m_NumberOfVerticies);
-	}*/
-
 	std::vector<float> RenderableObject::cubeVerticies = {
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
          0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
