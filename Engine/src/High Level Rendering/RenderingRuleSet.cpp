1eaff795ca6aa5a2a31186caa7a5b31277370b08
Ben-Bingham <ben@benbingham.ca>
2022-07-10 19:40:15 -0600

    Removed unnecessary comment

--- Engine/src/High Level Rendering/RenderingRuleSet.cpp
+++ Engine/src/High Level Rendering/RenderingRuleSet.cpp
@@ -1,15 +1,10 @@
 #include "High Level Rendering/Rule Sets/RenderingRuleSet.h"
 #include "High Level Rendering/ShaderCreator.h"
 #include "Core Systems/Resource Pipeline/TextFile.h"
-#include <High Level Rendering/RenderableObject.h>
+#include "High Level Rendering/Renderable Objects/RenderableObject.h"
 
 namespace Jade {
-	RenderingRuleSet::RenderingRuleSet(RuleSet rules) : m_Program(programInit()), m_ID(getNextID()), ruleSet(rules) {
-		/*m_Light.ambient = glm::vec3(0.2f, 0.2f, 0.2f);
-		m_Light.diffuse = glm::vec3(0.5f, 0.5f, 0.5f);
-		m_Light.specular = glm::vec3(1.0f, 1.0f, 1.0f);
-		m_Light.position = glm::vec3(1.2f, 1.0f, 2.0f);*/
-	}
+	RenderingRuleSet::RenderingRuleSet(RuleSet rules) : m_Program(programInit()), m_ID(getNextID()), ruleSet(rules) {}
 
 	void RenderingRuleSet::createProgram() {
 		m_Program.dispose();
