e7dd8829c95d75d828bcbf194153bffd7859d437
Ben-Bingham <ben@benbingham.ca>
2022-07-12 16:57:31 -0600

    Added support for custom shape type

--- Engine/include/High Level Rendering/Renderable Objects/RenderableObject.h
+++ Engine/include/High Level Rendering/Renderable Objects/RenderableObject.h
@@ -7,6 +7,7 @@
 #include "Low Level Rendering/Rendering Objects/VertexBufferObject.h"
 #include "Low Level Rendering/Rendering Objects/ElementBufferObject.h"
 #include "High Level Rendering/Transform.h"
+#include "Core Systems/Resource Pipeline/Model.h"
 
 namespace Jade {
 	enum Shape {
@@ -18,7 +19,13 @@ namespace Jade {
 	class RenderableObject {
 	public:
 		RenderableObject(RuleSet ruleset, Shape shape)
-			: m_VAO(), m_Shape(shape), m_VBO(vboInit()), m_RuleSet(ruleset), m_NumberOfIndicies(numberOfVerticiesInit()), m_EBO(eboInit()) {
+			: m_VAO(), m_Shape(shape), m_VBO(vboInit()), m_RuleSet(ruleset), m_NumberOfIndicies(numberOfVerticiesInit()), m_EBO(eboInit()), m_Mesh(std::vector<Vertex>{{glm::vec3(1.0), glm::vec3(1.0), glm::vec3(1.0)}}, std::vector<unsigned int>{1}, std::vector<TextureStruct>{ {1, "type", "Path"}}) {
+
+			init();
+		}
+
+		RenderableObject(RuleSet ruleset, Mesh mesh)
+			: m_VAO(), m_Shape(CUSTOM), m_VBO(vboInit()), m_RuleSet(ruleset), m_NumberOfIndicies(numberOfVerticiesInit()), m_EBO(eboInit()), m_Mesh(mesh) {
 
 			init();
 		}
@@ -37,6 +44,7 @@ namespace Jade {
 		void dispose() { m_VAO.dispose(); }
 
 	private:
+		Mesh m_Mesh;
 		Shape m_Shape;
 		VertexAttributeObject m_VAO;
 		VertexBufferObject m_VBO;
@@ -46,7 +54,7 @@ namespace Jade {
 		int m_NumberOfIndicies;
 
 		void init() {
-			if (m_Shape == CUBE || m_Shape == PYRAMID) {
+			if (m_Shape == CUBE || m_Shape == PYRAMID || m_Shape == CUSTOM) {
 				VertexAttributePointer positionData(3, GL_FLOAT, POSITION);
 				VertexAttributePointer normalData(3, GL_FLOAT, NORMAL);
 				VertexAttributePointer textureCords(2, GL_FLOAT, TEXTURE_CORDINATE);
@@ -66,6 +74,7 @@ namespace Jade {
 			default:
 			case CUBE: return VertexBufferObject(cubeVerticies);
 			case PYRAMID: return VertexBufferObject(pyramidVerticies);
+			case CUSTOM: return VertexBufferObject(m_Mesh.getVerticies());
 			}
 		}
 
@@ -74,6 +83,7 @@ namespace Jade {
 			default:
 			case CUBE: return ElementBufferObject(cubeIndicies);
 			case PYRAMID: return ElementBufferObject(pyramidIndicies);
+			case CUSTOM: return ElementBufferObject(m_Mesh.getIndicies());
 			}
 		}
 
@@ -82,6 +92,7 @@ namespace Jade {
 			default:
 			case CUBE: return 36;
 			case PYRAMID: return 18;
+			case CUSTOM: return m_Mesh.getIndicies().size();
 			}
 		}
 
