d78d04bccb6dc18f7e545827c5435bd7d1c2de1d
Ben-Bingham <ben@benbingham.ca>
2022-07-12 13:56:17 -0600

    Batch file now builds assimp if its not already been

--- CreateSolution.bat
+++ CreateSolution.bat
@@ -5,3 +5,23 @@ DEL /S *.vcxproj.user
 DEL /q .vs
 
 CALL Dependencies\premake\premake5.exe vs2022
+
+set result="false"
+
+if not exist Dependencies\assimp\assimp-vc143-mt.lib set result="true"
+if not exist Dependencies\assimp\assimp-vc143-mtd.lib set result="true"
+
+if %result%=="true" (
+	DEL /q Dependencies\assimp\assimp-vc143-mt.lib
+	DEL /q Dependencies\assimp\assimp-vc143-mtd.lib
+	cd Dependencies\assimp\assimp-5.2.4
+	RMDIR /s /q build
+	mkdir build
+	cd build
+	CALL cmake -DBUILD_SHARED_LIBS=OFF ../
+	CALL cmake --build . --config Release
+	CALL cmake --build . --config Debug
+	move lib\Release\assimp-vc143-mt.lib ..\..\
+	move lib\Debug\assimp-vc143-mtd.lib ..\..\
+)
+
