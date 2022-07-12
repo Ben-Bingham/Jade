1f8cdbc16a5d8d25a0635b2fef7dce693bc637b0
Ben-Bingham <ben@benbingham.ca>
2022-07-12 16:57:13 -0600

    Added additional constructor that takes in mesh

--- Engine/include/High Level Rendering/Renderable Objects/StandardRenderableObject.h
+++ Engine/include/High Level Rendering/Renderable Objects/StandardRenderableObject.h
@@ -2,12 +2,17 @@
 
 #include "RenderableObject.h"
 #include "High Level Rendering/MaterialCreator.h"
+#include "Core Systems/Resource Pipeline/Model.h"
 
 namespace Jade {
 	class StandardRenderable : public RenderableObject {
 	public:
 		StandardRenderable(Shape shape = CUBE) 
 			: RenderableObject(STANDARD, shape), m_Material(MaterialCreator::DefaultMaterial()) {}
+
+		StandardRenderable(Mesh mesh)
+			: RenderableObject(STANDARD, mesh), m_Material(MaterialCreator::DefaultMaterial()) {
+		}
 
 		void additionalRendering(const RenderingRuleSet& ruleSet) const override {
 			ruleSet.getProgram().setMaterial("material", m_Material);
