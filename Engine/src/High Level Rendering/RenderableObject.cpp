a618250afb0ff09446cc83a1255aceeeeae0f49b
Ben-Bingham <ben@benbingham.ca>
2022-07-12 18:57:01 -0600

    Modified primitive vertex data to work with Vertex struct

--- Engine/src/High Level Rendering/RenderableObject.cpp
+++ Engine/src/High Level Rendering/RenderableObject.cpp
@@ -1,5 +1,7 @@
 #include <vector>
 
+#include <glm/glm.hpp>
+
 #include "High Level Rendering/Renderable Objects/RenderableObject.h"
 
 namespace Jade {
@@ -13,39 +15,39 @@ namespace Jade {
         glDrawElements(GL_TRIANGLES, m_NumberOfIndicies, GL_UNSIGNED_INT, 0);
     }
 
-    std::vector<float> RenderableObject::cubeVerticies = {
-        -0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  1.0f,    0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  1.0f,    0.0f, 1.0f,
-         0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  1.0f,    1.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  1.0f,    1.0f, 1.0f,
+    std::vector<Vertex> cubeVerticies = {
+        Vertex{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3( 0.0f,  0.0f,  1.0f), glm::vec2(0.0f, 0.0f) },
+        Vertex{ glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3( 0.0f,  0.0f,  1.0f), glm::vec2(0.0f, 1.0f) },
+        Vertex{ glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3( 0.0f,  0.0f,  1.0f), glm::vec2(1.0f, 0.0f) },
+        Vertex{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3( 0.0f,  0.0f,  1.0f), glm::vec2(1.0f, 1.0f) },
 
-         0.5f, -0.5f,  0.5f,    1.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-         0.5f,  0.5f,  0.5f,    1.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,    1.0f,  0.0f,  0.0f,    1.0f, 0.0f,                         
-         0.5f,  0.5f, -0.5f,    1.0f,  0.0f,  0.0f,    1.0f, 1.0f,             
+        Vertex{ glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3( 1.0f,  0.0f,  0.0f), glm::vec2(0.0f, 0.0f) },
+        Vertex{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3( 1.0f,  0.0f,  0.0f), glm::vec2(0.0f, 1.0f) },
+        Vertex{ glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3( 1.0f,  0.0f,  0.0f), glm::vec2(1.0f, 0.0f) },
+        Vertex{ glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3( 1.0f,  0.0f,  0.0f), glm::vec2(1.0f, 1.0f) },
 
-        -0.5f, -0.5f, -0.5f,   -1.0f,  0.0f,  0.0f,    0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,   -1.0f,  0.0f,  0.0f,    0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,   -1.0f,  0.0f,  0.0f,    1.0f, 0.0f,                               
-        -0.5f,  0.5f,  0.5f,   -1.0f,  0.0f,  0.0f,    1.0f, 1.0f,
+        Vertex{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(0.0f, 0.0f) },
+        Vertex{ glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(0.0f, 1.0f) },
+        Vertex{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(1.0f, 0.0f) },
+        Vertex{ glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec2(1.0f, 1.0f) },
 
-        -0.5f,  0.5f,  0.5f,    0.0f,  1.0f,  0.0f,    0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,    0.0f,  1.0f,  0.0f,    0.0f, 1.0f,
-         0.5f,  0.5f,  0.5f,    0.0f,  1.0f,  0.0f,    1.0f, 0.0f,                                       
-         0.5f,  0.5f, -0.5f,    0.0f,  1.0f,  0.0f,    1.0f, 1.0f,              
+        Vertex{ glm::vec3(-0.5f,  0.5f,  0.5f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec2(0.0f, 0.0f) },
+        Vertex{ glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec2(0.0f, 1.0f) },
+        Vertex{ glm::vec3( 0.5f,  0.5f,  0.5f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec2(1.0f, 0.0f) },
+        Vertex{ glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec2(1.0f, 1.0f) },
 
-         0.5f, -0.5f, -0.5f,    0.0f,  0.0f, -1.0f,    0.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,    0.0f,  0.0f, -1.0f,    0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,    0.0f,  0.0f, -1.0f,    1.0f, 0.0f,                                            
-        -0.5f,  0.5f, -0.5f,    0.0f,  0.0f, -1.0f,    1.0f, 1.0f,             
+        Vertex{ glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec2(0.0f, 0.0f) },
+        Vertex{ glm::vec3( 0.5f,  0.5f, -0.5f), glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec2(0.0f, 1.0f) },
+        Vertex{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec2(1.0f, 0.0f) },
+        Vertex{ glm::vec3(-0.5f,  0.5f, -0.5f), glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec2(1.0f, 1.0f) },
 
-        -0.5f, -0.5f, -0.5f,    0.0f, -1.0f,  0.0f,    0.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,    0.0f, -1.0f,  0.0f,    0.0f, 1.0f,
-         0.5f, -0.5f, -0.5f,    0.0f, -1.0f,  0.0f,    1.0f, 0.0f,                                   
-         0.5f, -0.5f,  0.5f,    0.0f, -1.0f,  0.0f,    1.0f, 1.0f,
+        Vertex{ glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3( 0.0f, -1.0f,  0.0f), glm::vec2(0.0f, 0.0f) },
+        Vertex{ glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3( 0.0f, -1.0f,  0.0f), glm::vec2(0.0f, 1.0f) },
+        Vertex{ glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3( 0.0f, -1.0f,  0.0f), glm::vec2(1.0f, 0.0f) },
+        Vertex{ glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3( 0.0f, -1.0f,  0.0f), glm::vec2(1.0f, 1.0f) }
     };
 
-    std::vector<unsigned int> RenderableObject::cubeIndicies = {
+    std::vector<unsigned int> cubeIndicies = {
          0,  1,  2,
          1,  3,  2,
          4,  5,  6,
@@ -60,30 +62,32 @@ namespace Jade {
         21, 23, 22,
     };
 
-	std::vector<float> RenderableObject::pyramidVerticies = {
-		-0.5f, -0.5f,  0.5f,   0.0f, 0.44721f, 0.89443f,   0.0f, 0.0f,
-		 0.0f,  0.5f,  0.0f,   0.0f, 0.44721f, 0.89443f,   0.5f, 1.0f,
-         0.5f, -0.5f,  0.5f,   0.0f, 0.44721f, 0.89443f,   1.0f, 0.0f,
-                              
-		 0.5f, -0.5f,  0.5f,   0.89443f, 0.44721f, 0.0f,   0.0f, 0.0f,
-		 0.0f,  0.5f,  0.0f,   0.89443f, 0.44721f, 0.0f,   0.5f, 1.0f,
-		 0.5f, -0.5f, -0.5f,   0.89443f, 0.44721f, 0.0f,   1.0f, 0.0f,
-                              
-		-0.5f, -0.5f, -0.5f,   0.0f, 0.44721f, -0.89443f,  1.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f,   0.0f, 0.44721f, -0.89443f,  0.0f, 0.0f,
-		 0.0f,  0.5f,  0.0f,   0.0f, 0.44721f, -0.89443f,  0.5f, 1.0f,
-                              
-		-0.5f, -0.5f,  0.5f,  -0.89443f, 0.44721f, 0.0f,   1.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  -0.89443f, 0.44721f, 0.0f,   0.0f, 0.0f,
-		 0.0f,  0.5f,  0.0f,  -0.89443f, 0.44721f, 0.0f,   0.5f, 1.0f,
-                              
-		-0.5f, -0.5f,  0.5f,   0.0f, -1.0f, 0.0f,          0.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,   0.0f, -1.0f, 0.0f,          1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,   0.0f, -1.0f, 0.0f,          0.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f,   0.0f, -1.0f, 0.0f,          1.0f, 0.0f,
-	};
+    Mesh RenderableObject::cubeMesh(cubeVerticies, cubeIndicies, std::vector<TextureStruct>{{0, "", ""}});
 
-    std::vector<unsigned int> RenderableObject::pyramidIndicies = {
+    std::vector<Vertex> pyramidVerticies = {
+        Vertex{glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3( 0.0f, 0.44721f, 0.89443f),  glm::vec2(0.0f, 0.0f)},
+        Vertex{glm::vec3( 0.0f,  0.5f,  0.0f), glm::vec3( 0.0f, 0.44721f, 0.89443f),  glm::vec2(0.5f, 1.0f)},
+        Vertex{glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3( 0.0f, 0.44721f, 0.89443f),  glm::vec2(1.0f, 0.0f)},
+
+        Vertex{glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3( 0.89443f, 0.44721f, 0.0f),  glm::vec2(0.0f, 0.0f)},
+        Vertex{glm::vec3( 0.0f,  0.5f,  0.0f), glm::vec3( 0.89443f, 0.44721f, 0.0f),  glm::vec2(0.5f, 1.0f)},
+        Vertex{glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3( 0.89443f, 0.44721f, 0.0f),  glm::vec2(1.0f, 0.0f)},
+
+        Vertex{glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3( 0.0f, 0.44721f, -0.89443f), glm::vec2(1.0f, 0.0f)},
+        Vertex{glm::vec3( 0.5f, -0.5f, -0.5f), glm::vec3( 0.0f, 0.44721f, -0.89443f), glm::vec2(0.0f, 0.0f)},
+        Vertex{glm::vec3( 0.0f,  0.5f,  0.0f), glm::vec3( 0.0f, 0.44721f, -0.89443f), glm::vec2(0.5f, 1.0f)},
+
+        Vertex{glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3(-0.89443f, 0.44721f, 0.0f),  glm::vec2(1.0f, 0.0f)},
+        Vertex{glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3(-0.89443f, 0.44721f, 0.0f),  glm::vec2(0.0f, 0.0f)},
+        Vertex{glm::vec3( 0.0f,  0.5f,  0.0f), glm::vec3(-0.89443f, 0.44721f, 0.0f),  glm::vec2(0.5f, 1.0f)},
+
+        Vertex{glm::vec3(-0.5f, -0.5f,  0.5f), glm::vec3( 0.0f, -1.0f, 0.0f),         glm::vec2(0.0f, 1.0f)},
+        Vertex{glm::vec3( 0.5f, -0.5f,  0.5f), glm::vec3( 0.0f, -1.0f, 0.0f),         glm::vec2(1.0f, 1.0f)},
+        Vertex{glm::vec3(-0.5f, -0.5f, -0.5f), glm::vec3( 0.0f, -1.0f, 0.0f),         glm::vec2(0.0f, 0.0f)},
+        Vertex{glm::vec3( 0.5f, -0.5f, -0.5f),  glm::vec3(0.0f, -1.0f, 0.0f),         glm::vec2(1.0f, 0.0f)}
+    };
+
+    std::vector<unsigned int> pyramidIndicies = {
         0,  1,  2,
         3,  4,  5,
         6,  7,  8,
@@ -91,4 +95,6 @@ namespace Jade {
         12, 13, 14,
         13, 15, 14
     };
+
+    Mesh RenderableObject::pyramidMesh(pyramidVerticies, pyramidIndicies, std::vector<TextureStruct>{ {0, "", ""}});
 }
