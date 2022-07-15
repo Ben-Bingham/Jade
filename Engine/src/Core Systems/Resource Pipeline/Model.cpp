6ec2c50e71fc17c13646d31ece48951cfeccfe4a
Ben-Bingham <ben@benbingham.ca>
2022-07-14 19:45:31 -0600

    implemented create and load internal methods

--- Engine/src/Core Systems/Resource Pipeline/Model.cpp
+++ Engine/src/Core Systems/Resource Pipeline/Model.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <fstream>
+#include <sstream>
+#include <iostream>
 
 #include "Jade.h"
 
@@ -14,19 +16,23 @@ namespace Jade {
 		fileName += m_Directory.c_str();
 		fileName += '\\';
 		fileName += m_Name;
-		fileName += internalExtension;
+		fileName += internalBinaryExtension;
+
 		std::ifstream f(fileName);
 		if (f.good()) {
-			loadFromInternal();
+			std::string fileNameNoExtension = "";
+			fileNameNoExtension += m_Directory.c_str();
+			fileNameNoExtension += '\\';
+			fileNameNoExtension += m_Name;
+			loadFromInternal(fileNameNoExtension);
 		}
 		else {
 			loadFromRaw();
 		}
+		f.close();
 	}
 
 	void Model::loadFromRaw() {
-		LOGGER.log("Loading raw", INFO);
-
 		Assimp::Importer importer;
 		const aiScene* scene = importer.ReadFile(m_Path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_FlipWindingOrder);
 
@@ -43,18 +49,118 @@ namespace Jade {
 
 		loadTextures(scene);
 
-		createInternal(*this);
+		createInternal();
 	}
 
-	void Model::createInternal(const Model& model) {
-		LOGGER.log("Creating internal", INFO);
-		TextFile file(m_Directory + m_Name + internalExtension, "Internal Model");
-		//TODO
+	void Model::createInternal() {
+		std::string message = "";
+		message += "Loading make take longer than usual, certain models need to be formated, including: ";
+		message += m_Name;
+		LOGGER.log(message, INFO);
+
+		std::ofstream outputFile(m_Directory + '\\' + m_Name + internalBinaryExtension, std::ios::out | std::ios::binary);
+		if (outputFile.is_open()) {
+
+			size_t size = m_Meshes.size();
+			outputFile.write((char*)&size, sizeof(size_t));
+			std::vector<Mesh>::iterator it;
+			for (it = m_Meshes.begin(); it != m_Meshes.end(); it++) {
+
+				std::vector<Vertex> verticies = it->getVerticies();
+				size = verticies.size() * sizeof(Vertex);
+				outputFile.write((char*)&size, sizeof(size_t));
+				outputFile.write((char*)&verticies[0], size);
+
+				size_t sizeVert = sizeof(Vertex);
+				size_t sizeUI = sizeof(unsigned int);
+
+				std::vector<unsigned int> indicies = it->getIndicies();
+				size = indicies.size() * sizeof(unsigned int);
+				outputFile.write((char*)&size, sizeof(size_t));
+				outputFile.write((char*)&indicies[0], size);
+			}
+
+			size = m_Images.size();
+			outputFile.write((char*)&size, sizeof(size_t));
+			std::vector<ImageMetaData>::iterator it2;
+			for (it2 = m_Images.begin(); it2 != m_Images.end(); it2++) {
+				ImageMetaData image = *it2;
+				outputFile.write((char*)&image.type, sizeof(aiTextureType));
+
+				Image pic = image.image;
+				size = pic.getWidth();
+				outputFile.write((char*)&size, sizeof(size_t));
+				size = pic.getHeight();
+				outputFile.write((char*)&size, sizeof(size_t));
+				size = pic.getChannels();
+				outputFile.write((char*)&size, sizeof(size_t));
+
+				outputFile.write((char*)&pic.getContent()[0], pic.getWidth() * pic.getHeight() * pic.getChannels() * sizeof(unsigned char));
+			}
+
+			outputFile.close();
+		}
+		else {
+			LOGGER.log("Unable to open binary file: " + m_Directory + m_Name + internalBinaryExtension, ERROR);
+		}
 	}
 
-	void Model::loadFromInternal() {
-		LOGGER.log("Loading internally", INFO);
-		//TODO
+	void Model::loadFromInternal(const std::string& fileName) {
+		std::ifstream inputFileStream(fileName + internalBinaryExtension, std::ios::in | std::ios::binary);
+		if (inputFileStream.is_open()) {
+			size_t size = 0;
+			size_t numberOfMeshes = 0;
+			inputFileStream.read((char*)&numberOfMeshes, sizeof(size_t));
+
+			std::vector<Mesh> meshes;
+
+			for (unsigned int i = 0; i < numberOfMeshes; i++) {
+				inputFileStream.read((char*)&size, sizeof(size_t));
+				
+				std::vector<Vertex> verticies(size / sizeof(Vertex));
+				inputFileStream.read((char*)&verticies[0], size);
+
+				inputFileStream.read((char*)&size, sizeof(size_t));
+
+				std::vector<unsigned int> indicies(size / sizeof(unsigned int));
+				inputFileStream.read((char*)&indicies[0], size);
+
+				Mesh mesh(verticies, indicies);
+				meshes.push_back(mesh);
+			}
+
+			size_t numberOfTextures = 0;
+			inputFileStream.read((char*)&numberOfTextures, sizeof(size_t));
+
+			std::vector<ImageMetaData> images;
+
+			for (unsigned int i = 0; i < numberOfTextures; i++) {
+				aiTextureType imageType;
+				int width = 0, height = 0, channels = 0;
+
+				inputFileStream.read((char*)&imageType, sizeof(aiTextureType));
+				inputFileStream.read((char*)&size, sizeof(size_t));
+				width = (int)size;
+				inputFileStream.read((char*)&size, sizeof(size_t));
+				height = (int)size;
+				inputFileStream.read((char*)&size, sizeof(size_t));
+				channels = (int)size;
+
+				unsigned char* imageData = new unsigned char[width * height * channels * sizeof(unsigned char)];
+
+				inputFileStream.read((char*)imageData, width * height * channels * sizeof(unsigned char));
+
+				Image image(imageData, width, height, channels);
+				images.push_back(ImageMetaData{ image, imageType });
+			}
+
+			m_Meshes = meshes;
+			m_Images = images;
+			inputFileStream.close();
+		}
+		else {
+			LOGGER.log("Unable to open binary file: " + fileName, ERROR);
+		}
 	}
 
 	void Model::processNode(aiNode* node, const aiScene* scene) {
@@ -113,26 +219,26 @@ namespace Jade {
 		int val = scene->mNumMaterials;
 		for (unsigned int i = 0; i < scene->mNumMaterials; i++) {
 			aiMaterial* mat = scene->mMaterials[i];
-			loadMaterialTextures(mat, aiTextureType_DIFFUSE, m_Textures);
-			loadMaterialTextures(mat, aiTextureType_SPECULAR, m_Textures);
+			loadMaterialTextures(mat, aiTextureType_DIFFUSE, m_Images);
+			loadMaterialTextures(mat, aiTextureType_SPECULAR, m_Images);
 		}
 	}
 
-	void Model::loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::vector<MetaDataTexture>& textureList) {
+	void Model::loadMaterialTextures(aiMaterial* mat, aiTextureType type, std::vector<ImageMetaData>& textureList) {
 		for (unsigned int j = 0; j < mat->GetTextureCount(type); j++) {
 			aiString str;
 			mat->GetTexture(type, j, &str);
 
-			std::vector<MetaDataTexture>::iterator it;
+			std::vector<ImageMetaData>::iterator it;
 			for (it = textureList.begin(); it != textureList.end(); it++) {
 				if (it->type == type) {
 					return;
 				}
 			}
 
-			textureList.push_back(MetaDataTexture{Texture(Image(m_Directory + '\\' + str.C_Str())), type});
+			textureList.push_back(ImageMetaData{Image(m_Directory + '\\' + str.C_Str()), type});
 		}
 	}
 
-	std::string Model::internalExtension = ".JadeModel";
+	std::string Model::internalBinaryExtension = ".JadeModel";
 }
