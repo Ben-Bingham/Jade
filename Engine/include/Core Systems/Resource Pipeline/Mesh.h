8781f9ee37517cc0596bb24641c4b357a5dbc6e6
Ben-Bingham <ben@benbingham.ca>
2022-07-14 19:41:01 -0600

    modified getVerticiesAsFloat function to be much more efficient

--- Engine/include/Core Systems/Resource Pipeline/Mesh.h
+++ Engine/include/Core Systems/Resource Pipeline/Mesh.h
@@ -11,19 +11,6 @@ namespace Jade {
 		glm::vec3 position;
 		glm::vec3 normal;
 		glm::vec2 textureCordinates;
-
-		std::vector<float> getAsFloatVector() {
-			std::vector<float> floats;
-			floats.push_back(position.x);
-			floats.push_back(position.y);
-			floats.push_back(position.z);
-			floats.push_back(normal.x);
-			floats.push_back(normal.y);
-			floats.push_back(normal.z);
-			floats.push_back(textureCordinates.x);
-			floats.push_back(textureCordinates.y);
-			return floats;
-		}
 	};
 
 	class Mesh {
@@ -32,16 +19,20 @@ namespace Jade {
 			: m_Verticies(verticies), m_Indicies(indicies) {
 		}
 
+		Mesh() : m_Verticies(), m_Indicies() {}
+
 		std::vector<float> getVerticiesAsFloatVector() const {
-			std::vector<float> verticies;
-			std::vector<float> vert;
-			for each (Vertex vertex in m_Verticies) {
-				vert = vertex.getAsFloatVector();
-				verticies.insert(verticies.end(), vert.begin(), vert.end());
-			}
+			int numberOfFloatsInVertex = 8;
+
+			std::vector<float> verticies(m_Verticies.size() * numberOfFloatsInVertex);
+
+			memcpy(&verticies[0], &m_Verticies[0], m_Verticies.size() * numberOfFloatsInVertex * sizeof(float));
+
 			return verticies;
 		}
+
 		std::vector<unsigned int> getIndicies() const { return m_Indicies; }
+		std::vector<Vertex> getVerticies() const { return m_Verticies; }
 
 	private:
 		std::vector<Vertex> m_Verticies;
