1ca1f5c57e0e5598c58ef4b54ed306c2284a6264
Ben-Bingham <ben@benbingham.ca>
2022-07-20 14:50:20 -0600

    Created Keyboard class that can be used to get info on keys

---
+++ Engine/include/Core Systems/Human Interface Devices/Keyboard.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <GLFW/glfw3.h>
+
+namespace Jade {
+	enum Key {
+		KEY_W, KEY_A, KEY_S, KEY_D, KEY_SPACE, KEY_LEFT_SHIFT //TODO add all keys
+	};
+
+	enum KeyState {
+		KEY_PRESSED, KEY_RELEASED, KEY_REPEAT
+	};
+
+	class Keyboard {
+	public:
+		Keyboard(Window* window) : m_Window(window) {}
+
+		bool getKeyPressed(const Key key) const {
+			return glfwGetKey(m_Window->getWindow(), keyToGLFW(key)) == stateToGLFW(KEY_PRESSED);
+		}
+
+		bool getKeyReleased(const Key key) const {
+			return glfwGetKey(m_Window->getWindow(), keyToGLFW(key)) == stateToGLFW(KEY_RELEASED);
+		}
+
+		bool getKeyHeld(const Key key) const { //TODO does not seem to work
+			return glfwGetKey(m_Window->getWindow(), keyToGLFW(key)) == stateToGLFW(KEY_REPEAT);
+		}
+
+	private:
+		Window* m_Window;
+
+		int keyToGLFW(const Key key) const {
+			switch (key) {
+			default:
+			case KEY_W:             return GLFW_KEY_W;
+			case KEY_A:             return GLFW_KEY_A;
+			case KEY_S:             return GLFW_KEY_S;
+			case KEY_D:             return GLFW_KEY_D;
+			case KEY_SPACE:         return GLFW_KEY_SPACE;
+			case KEY_LEFT_SHIFT:    return GLFW_KEY_LEFT_SHIFT;
+			}
+		}
+
+		int stateToGLFW(const KeyState state) const {
+			switch (state) {
+			default:
+			case KEY_PRESSED:    return GLFW_PRESS;
+			case KEY_RELEASED:   return GLFW_RELEASE;
+			case KEY_REPEAT:     return GLFW_REPEAT;
+			}
+		}
+	};
+}
