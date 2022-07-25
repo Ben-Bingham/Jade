ac2b4a7da339a5d80fceffd23e2b5df5e52db108
Ben-Bingham <ben@benbingham.ca>
2022-07-25 17:05:04 -0600

    Image class uses std::vector to store data

--- Engine/include/Core Systems/Resource Pipeline/Image.h
+++ Engine/include/Core Systems/Resource Pipeline/Image.h
@@ -1,42 +1,44 @@
 #pragma once
 #include <sstream>
+#include <vector>
 
 #include "Jade.h"
 
 #include "stb_image.h"
+#include "stb_image_write.h"
+#include "High Level Rendering/Colour.h"
 
 namespace Jade {
 	class Image {
 	public:
-		Image(const std::string& path, bool flipVertically = true) : m_Path(path), m_Stbi(true) {
+		Image(const std::string& path, bool flipVertically = true) : m_Path(path) {
 			stbi_set_flip_vertically_on_load(flipVertically);
 
-			m_Content = stbi_load(m_Path.c_str(), &m_Width, &m_Height, &m_Channels, 0);
+			unsigned char* data = stbi_load(m_Path.c_str(), &m_Width, &m_Height, &m_Channels, 0);
+			m_Content.resize(m_Width * m_Height * m_Channels);
 
-			if (!m_Content) {
-				LOG("Image: " + m_Path + "Failed to load.", Jade::ERROR);
-				this->free();
+			memcpy(&m_Content[0], data, m_Content.size());
+
+			if (!data) {
+				LOG("Image: " + m_Path + "Failed to load. ", Jade::ERROR);
 			}
+			stbi_image_free(data);
 		}
 
 		Image(unsigned char* content, int width, int height, int channels) 
 			: m_Content(content), m_Width(width), m_Height(height), m_Channels(channels), m_Stbi(false) {}
 
-		unsigned char* getContent() const { return m_Content; }
+		std::vector<unsigned char> getContent() const { return m_Content; }
 		int getWidth() const { return m_Width; }
 		int getHeight() const { return m_Height; }
 		int getChannels() const { return m_Channels; }
 		std::string getPath() const { return m_Path; }
-		bool getStbi() const { return m_Stbi; }
-
-		void free() { if (m_Stbi) { stbi_image_free(m_Content); } else { delete[] m_Content; } }
 
 	private:
-		unsigned char* m_Content;
+		std::vector<unsigned char> m_Content;
 		std::string m_Path;
 		int m_Width;
 		int m_Height;
 		int m_Channels;
-		bool m_Stbi;
 	};
 }
