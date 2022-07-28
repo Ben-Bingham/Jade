57aa78e3b96ac76a6d61db3aa323c39f94cf47d2
Ben-Bingham <ben@benbingham.ca>
2022-07-28 17:19:25 -0600

    Level 1 will now end after holding enter for a bit

--- Sandbox/include/Level1.h
+++ Sandbox/include/Level1.h
@@ -1,9 +1,41 @@
 #pragma once
-#include "Entity Component System/Scene.h"
+#include "Engine Structure/Scene.h"
+#include "Engine Structure/Engine.h"
 
 class Level1 : public Jade::Scene {
 public:
 	Level1() {}
 
 	unsigned int playerScore{ 0 };
+
+	Jade::Model backpack{ "assets\\models\\backpack\\backpack.obj", true };
+	Jade::Gameobject testCube{};
+
+	Jade::PhongRenderingComponent backpackRenderer{ &backpack };
+
+	void Begin() override {
+		backpackRenderer.translate(glm::vec3(0, 0, -10));
+		testCube.addComponent(&backpackRenderer);
+		addGameobject(&testCube);
+	}
+
+	bool keyHit{ false };
+	bool keyReleased{ false };
+
+	void Update() override {
+		LOG(std::to_string(playerScore));
+		if (Jade::gKeyboard.getKeyPressed(Jade::KEY_ENTER)) {
+			playerScore++;
+		}
+
+
+		if (playerScore > 100) {
+			stop();
+		}
+	}
+
+	void Cleanup() override {
+		playerScore = 0;
+		isRunning = true;
+	}
 };
