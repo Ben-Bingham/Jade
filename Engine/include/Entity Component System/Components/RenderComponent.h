c763cdea8ac6ae5e5e1e2cc29c9358cf58f6b03a
Ben-Bingham <ben@benbingham.ca>
2022-07-30 21:22:07 -0600

    Removed translate funciton

--- Engine/include/Entity Component System/Components/RenderComponent.h
+++ Engine/include/Entity Component System/Components/RenderComponent.h
@@ -3,20 +3,12 @@
 #include <glm/glm.hpp>
 
 #include "Entity Component System/Components/Component.h"
-#include <High Level Rendering/Renderable Objects/RenderableObject.h>
+#include "High Level Rendering/Renderable Objects/RenderableObject.h"
 
 namespace Jade {
 	class RenderComponent : public Component {
 	public:
 		virtual ~RenderComponent() = 0;
-
-		void translate(glm::vec3 vector) {
-			for (RenderableObject* renderable : renderables) {
-				renderable->getTransform().position.x += vector.x;
-				renderable->getTransform().position.y += vector.y;
-				renderable->getTransform().position.z += vector.z;
-			}
-		}
 
 		std::vector<RenderableObject*> renderables;
 	};
