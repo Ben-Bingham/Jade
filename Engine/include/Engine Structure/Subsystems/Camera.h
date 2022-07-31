9baeafe055735cd5d4aa079a8270198176f3fd1d
Ben-Bingham <ben@benbingham.ca>
2022-07-30 21:46:56 -0600

    removed unnecessary struct

--- Engine/include/Engine Structure/Subsystems/Camera.h
+++ Engine/include/Engine Structure/Subsystems/Camera.h
@@ -7,11 +7,6 @@
 #include "Entity Component System/Gameobjects/Gameobject.h"
 
 namespace Jade {
-	struct CameraInfo {
-		float* fov;
-		glm::vec3* position;
-	};
-
 	class Camera : public Gameobject {
 	public:
 		Camera(float fov = 45.0f, glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 worldUp = glm::vec3(0.0f, 1.0f, 0.0f));
