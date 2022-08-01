e1b39e5ce95a1905f9b8dc7c39f9d3e677563990
Ben-Bingham <ben@benbingham.ca>
2022-08-01 10:13:12 -0600

    Implemented a demo scene

---
+++ Sandbox/include/DemoScene.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Engine Structure/Scene.h"
+
+class DemoScene : public Jade::Scene {
+public:
+	Jade::Gameobject demoCube{};
+	Jade::Gameobject lightBox{};
+	Jade::PointLight light{};
+
+	Jade::PhongRenderingComponent demoCubeRenderingComponent{ Jade::CUBE, Jade::MaterialCreator::DefaultMaterial() };
+	Jade::SolidRenderingComponent lightBoxRenderingComponent{ Jade::CUBE, Jade::Colour(255, 255, 255) };
+
+	void Begin() override {
+		demoCube.addComponent(demoCubeRenderingComponent);
+		lightBox.addComponent(lightBoxRenderingComponent);
+
+		lightBox.getComponent<Jade::Transform>()->position = glm::vec3(2.0f, 4.0f, -2.0f);
+		lightBox.getComponent<Jade::Transform>()->scale = glm::vec3(0.2f);
+		light.getComponent<Jade::Transform>()->position = glm::vec3(1.0f, 2.0f, -1.0f);
+
+		addGameobject(demoCube);
+		addGameobject(lightBox);
+		addLight(light);
+	}
+};
--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -1,15 +1,5 @@
-#include <glm/glm.hpp>
-
-#include "High Level Rendering/PShaders/PShader.h"
-#include "High Level Rendering/PShaders/PhongShader.h"
-#include "High Level Rendering/PShaders/SolidShader.h"
-#include "High Level Rendering/Renderable Objects/SolidRenderableObject.h"
-#include "High Level Rendering/Renderable Objects/PhongRenderableObject.h"
-#include "High Level Rendering/MaterialCreator.h"
-#include "Entity Component System/GameObjects/Gameobject.h"
-#include "Entity Component System/Components/PhongRenderingComponent.h"
 #include "Engine Structure/Engine.h"
-#include "Level1.h"
+#include "DemoScene.h"
 #include "Entity Component System/Components/FPSController.h"
 
 int main() {
@@ -22,11 +12,10 @@ int main() {
 	Jade::FPSController fpsController;
 	camera.addComponent(fpsController);
 
-	Level1 lvl1{};
-	lvl1.addGameobject(camera);
+	DemoScene scene{};
+	scene.addGameobject(camera);
 
-	engine.LoadScene(lvl1);
-	engine.LoadScene(lvl1);
+	engine.LoadScene(scene);
 
 	engine.ShutDown();
 	return 0;
